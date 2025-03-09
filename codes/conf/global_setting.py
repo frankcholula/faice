ce6f5a47ea05151bf180c468a38d1c894f810d00
Ngene <hplxg@hotmail.com>
2025-03-09 19:41:47 +0000

    Feat: adjust parameters

--- codes/conf/global_setting.py
+++ codes/conf/global_setting.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # ********************************************* MODEL SETTING ********************************************* #
 @dataclass
 class TrainingConfig:
-    image_size = 256  # the generated image resolution
-    train_batch_size = 5
-    eval_batch_size = 5  # how many images to sample during evaluation
-    num_epochs = 20
+    image_size = 128  # the generated image resolution
+    train_batch_size = 24
+    eval_batch_size = 16  # how many images to sample during evaluation
+    num_epochs = 50
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
     lr_warmup_steps = 500
