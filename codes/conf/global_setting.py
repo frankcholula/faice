73f460a2592b0beba4f95132a8dc65c8ea3f9996
Ngene <hplxg@hotmail.com>
2025-03-09 15:44:59 +0000

    Fix: change parameters

--- codes/conf/global_setting.py
+++ codes/conf/global_setting.py
@@ -22,7 +22,7 @@ class TrainingConfig:
     image_size = 128  # the generated image resolution
     train_batch_size = 2
     eval_batch_size = 16  # how many images to sample during evaluation
-    num_epochs = 50
+    num_epochs = 2
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
     lr_warmup_steps = 500
--- codes/core/__init__.py
+++ codes/core/__init__.py
@@ -5,3 +5,12 @@
 @File : __init__.py.py
 @Project : faice
 """
+# from datasets import load_dataset
+# from codes.conf.global_setting import BASE_DIR, config
+#
+# config.dataset_name = "huggan/smithsonian_butterflies_subset"
+# dataset = load_dataset(config.dataset_name, split="train")
+#
+# sample_image = dataset[0]["images"].unsqueeze(0)
+# print("Input shape:", sample_image.shape)
+
