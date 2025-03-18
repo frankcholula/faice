58c2fbbfec2388978c450a150d58153a7abb0b1a
Ngene <hplxg@hotmail.com>
2025-03-18 11:26:47 +0000

    Feat: train with LoRA

--- codes/core/U_Net2D.py
+++ codes/core/U_Net2D.py
@@ -55,7 +55,7 @@ def unet2d_model():
         in_channels=3,
         out_channels=3,
         layers_per_block=2,
-        block_out_channels=(64, 128, 256, 512),
+        block_out_channels=(128, 128, 256, 256, 512, 512),
         down_block_types=(
         "DownBlock2D", "DownBlock2D", "DownBlock2D", "DownBlock2D", "AttnDownBlock2D", "DownBlock2D"),
         up_block_types=("UpBlock2D", "AttnUpBlock2D", "UpBlock2D", "UpBlock2D", "UpBlock2D", "UpBlock2D")
