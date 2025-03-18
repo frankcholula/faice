a80bb8484731b86438ee6b99e1739cb45b1bfb9c
Ngene <hplxg@hotmail.com>
2025-03-18 15:08:59 +0000

    Feat: train with pre-train model

--- codes/core/U_Net2D.py
+++ codes/core/U_Net2D.py
@@ -26,33 +26,33 @@ from codes.conf.global_setting import BASE_DIR, config
 
 
 def unet2d_model():
-    # model = UNet2DModel(
-    #     sample_size=config.image_size,  # the target image resolution
-    #     in_channels=3,  # the number of input channels, 3 for RGB images
-    #     out_channels=3,  # the number of output channels
-    #     layers_per_block=2,  # how many ResNet layers to use per UNet block
-    #     block_out_channels=(128, 128, 256, 256, 512, 512),
-    #     # the number of output channels for each UNet block
-    #     down_block_types=(
-    #         "DownBlock2D",  # a regular ResNet downsampling block
-    #         "DownBlock2D",
-    #         "DownBlock2D",
-    #         "DownBlock2D",
-    #         "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-    #         "DownBlock2D",
-    #     ),
-    #     up_block_types=(
-    #         "UpBlock2D",  # a regular ResNet upsampling block
-    #         "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-    #         "UpBlock2D",
-    #         "UpBlock2D",
-    #         "UpBlock2D",
-    #         "UpBlock2D",
-    #     ),
-    # )
+    model = UNet2DModel(
+        sample_size=config.image_size,  # the target image resolution
+        in_channels=3,  # the number of input channels, 3 for RGB images
+        out_channels=3,  # the number of output channels
+        layers_per_block=2,  # how many ResNet layers to use per UNet block
+        block_out_channels=(128, 128, 256, 256, 512, 512),
+        # the number of output channels for each UNet block
+        down_block_types=(
+            "DownBlock2D",  # a regular ResNet downsampling block
+            "DownBlock2D",
+            "DownBlock2D",
+            "DownBlock2D",
+            "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+            "DownBlock2D",
+        ),
+        up_block_types=(
+            "UpBlock2D",  # a regular ResNet upsampling block
+            "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+            "UpBlock2D",
+            "UpBlock2D",
+            "UpBlock2D",
+            "UpBlock2D",
+        ),
+    )
 
     # Initialize model with LoRA weights
-    model = UNet2DModel.from_pretrained(
+    model = model.from_pretrained(
         "google/ddpm-celebahq-256",  # Base model
     )
     # model.load_attn_procs("sassad/face-lora")  # Load LoRA weights
