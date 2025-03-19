d9a71cb9747c8435bceaa0ad24439af543957491
Ngene <hplxg@hotmail.com>
2025-03-19 01:12:05 +0000

    Feat: change the data augumention

--- codes/data_exploration/preprocess_data.py
+++ codes/data_exploration/preprocess_data.py
@@ -22,7 +22,7 @@ def transform(examples):
         [
             transforms.Resize((config.image_size, config.image_size)),
             transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             transforms.ToTensor(),
             transforms.Normalize([0.5], [0.5]),
         ]
