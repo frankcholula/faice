aa173a96ab188601bdc1650327aca6811edbb560
Ngene <hplxg@hotmail.com>
2025-03-26 13:40:09 +0000

    Feat: test 3 piplines

--- codes/core/train.py
+++ codes/core/train.py
@@ -65,6 +65,8 @@ def make_grid(images, rows, cols):
     w, h = images[0].size
     grid = Image.new("RGB", size=(cols * w, rows * h))
     for i, image in enumerate(images):
+        if i >= 16:
+            break
         grid.paste(image, box=(i % cols * w, i // cols * h))
     return grid
 
