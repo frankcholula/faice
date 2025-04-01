bab87e5cdac95f85fd19f8c01ebdc46f195efbb3
frankcholula <lu.phrank@gmail.com>
2025-04-01 12:18:17 +0100

    added some args.

---
+++ code/args.py
@@ -0,0 +1,68 @@
+import argparse
+from typing import Dict, List
+from conf.training_config import ButterflyConfig, FaceConfig
+
+
+def get_available_scheduelrs() -> List:
+    return ["ddpm", "ddim", "pndm", "lms"]
+
+
+def get_available_modles() -> List:
+    return ["unet", "unet3d", "controlnet"]
+
+
+def get_available_pipelines() -> List:
+    return ["ddpm", "ddim"]
+
+
+def get_available_datasets() -> Dict:
+    return {
+        "butterfly": ButterflyConfig,
+        "face": FaceConfig,
+    }
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Training arguments")
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="face",
+        choices=get_available_datasets(),
+        help="Dataset to use for training",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="unet",
+        choices=get_available_modles(),
+        help="Model to use for training",
+    )
+    parser.add_argument(
+        "--pipeline",
+        type=str,
+        default="ddpm",
+        choices=get_available_pipelines(),
+        help="Pipeline to use for training",
+    )
+
+    parser.add_argument(
+        "--scheduler",
+        type=str,
+        default="ddpm",
+        choices=get_available_scheduelrs(),
+        help="Scheduler to use for training",
+    )
+
+    parser.add_argument(
+        "--batch_size", type=int, default=16, help="Batch size for training"
+    )
+    parser.add_argument(
+        "--eval_batch_size", type=int, default=16, help="Batch size for evaluation"
+    )
+    parser.add_argument(
+        "--learning_rate", type=float, default=1e-4, help="Learning rate for training"
+    )
+    parser.add_argument(
+        "--num_epochs", type=int, default=20, help="Number of epochs for training"
+    )
