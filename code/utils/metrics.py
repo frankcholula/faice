a007d4feb0d67b7fe3089df9064c31f0c75f474d
Frank Lu <lu.phrank@gmail.com>
2025-04-03 15:16:31 +0100

    added debug message.

--- code/utils/metrics.py
+++ code/utils/metrics.py
@@ -6,6 +6,10 @@ from torchvision.transforms import functional as F
 from PIL import Image
 from tqdm.auto import tqdm
 from huggingface_hub import whoami, HfFolder
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def make_grid(images, rows, cols):
@@ -55,9 +59,19 @@ def calculate_fid_score(config, pipeline, test_dataloader, device=None):
     def preprocess_image(image, real):
         # Process fake images, need to convert to BCHW but no need to rescale.
         if real:
+            logger.debug("Processing real image...")
+            logger.debug(f"  Image shape: {image.shape} and dtype: {image.dtype}")
+            logger.debug(f"  Image min: {image.min()} and max: {image.max()}")
+            is_bchw = len(image.shape) == 4 and image.shape[1] == 3
+            logger.debug(f"  Format: {'BCHW' if is_bchw else 'NOT BCHW'}")
             image = (image + 1.0) / 2.0
         else:
             # Process fake images, need to convert to BCHW but no need to rescale.
+            logger.debug("Processing fake image...")
+            logger.debug(f"  Image shape: {image.shape} and dtype: {image.dtype}")
+            logger.debug(f"  Image min: {image.min()} and max: {image.max()}")
+            is_bchw = len(image.shape) == 4 and image.shape[1] == 3
+            logger.debug(f"  Format: {'BCHW' if is_bchw else 'NOT BCHW'}")
             image = torch.tensor(image, device=device)
             image = image.permute(0, 3, 1, 2)  # Convert from BHWC to BCHW format
         image = F.center_crop(image, (config.image_size, config.image_size))
@@ -88,7 +102,7 @@ def calculate_fid_score(config, pipeline, test_dataloader, device=None):
 
     # Compute final FID score
     fid_score = fid.compute().item()
-    print(f"FID Score: {fid_score}")
+    logger.info(f"FID Score: {fid_score}")
     return fid_score
 
 
