66b0e6ad838c4c8a218d103374edb9a4109b2123
Ngene <hplxg@hotmail.com>
2025-04-06 12:21:42 +0100

    Feat: add test for resize image

--- src/data_exploration/preprocess_data.py
+++ src/data_exploration/preprocess_data.py
@@ -5,10 +5,12 @@
 @File : preprocess_data.py
 @Project : faice
 """
-
+import torch
+import numpy as np
 from torchvision import transforms
 import matplotlib.pyplot as plt
 from datasets import load_dataset
+from PIL import Image
 
 from conf.global_setting import (BASE_DIR)
 from conf.model_config import model_config as config
@@ -53,7 +55,29 @@ def inspect_data(data):
     fig.show()
 
 
+def change_image_size(img_path, save_path):
+    dataset = load_dataset("imagefolder", data_dir=img_path, split="train")
+    # Convert all the image size into image_size*image_size
+    for i, image in enumerate(dataset):
+        image = image["image"]
+        transform = transforms.ToTensor()
+        image = transform(image)
+        image = torch.tensor(image)
+        transform = transforms.Resize((config.image_size, config.image_size))
+        resized_image = transform(image)
+        resized_image = resized_image.permute(1, 2, 0)
+        resized_image = resized_image.numpy()
+        images_uint8 = (resized_image * 255).astype(np.uint8)
+        images_uint8 = Image.fromarray(images_uint8)
+        images_uint8.save(save_path + "/" + str(i) + ".jpg")
+
+
 if __name__ == '__main__':
-    data_path = BASE_DIR + "/data/celeba_hq_256/"
-    dataset = get_data(data_path, False)
-    inspect_data(dataset)
+    # data_path = BASE_DIR + "/data/celeba_hq_256/"
+    # dataset = get_data(data_path, False)
+    # inspect_data(dataset)
+
+    test_dir = BASE_DIR + "/data/celeba_hq_split/test30"
+    save_dir = BASE_DIR + "/data/celeba_hq_split/test30_128"
+    change_image_size(test_dir, save_dir)
+
