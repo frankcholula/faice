9ce6b1dea8feba34ce0972e4b6a8b26923ac7a4c
frankcholula <lu.phrank@gmail.com>
2025-04-06 22:07:05 +0100

    more descriptive name.

--- code/experiments/augmentation.sh
+++ code/experiments/augmentation.sh
@@ -3,7 +3,7 @@ python main.py \
     --scheduler ddpm \
     --beta_schedule linear \
     --image_size 128 \
-    --num_epochs 1 \
-    --train_batch_size 32 \
+    --num_epochs 500 \
+    --train_batch_size 64 \
     --gblur \
-    --wandb_run_name ziyu-test-sh-augmentation
+    --wandb_run_name ddpm-linear-face-129-gblur
