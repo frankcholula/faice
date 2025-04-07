eb533dd9d294166f0631e2b51bb73cbf7fdb94b2
Ngene <hplxg@hotmail.com>
2025-04-07 09:44:19 +0100

    feat: test 500 epoches

--- code/train.sh
+++ code/train.sh
@@ -3,7 +3,7 @@ python main.py \
     --scheduler ddpm \
     --beta_schedule linear \
     --image_size 128 \
-    --num_epochs 2 \
+    --num_epochs 500 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
     --wandb_run_name liang_ddpm_linear \
