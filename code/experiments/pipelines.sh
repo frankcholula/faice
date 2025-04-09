ba9a55096c0e081e271275cd1cb723217ec11e62
Ngene <hplxg@hotmail.com>
2025-04-09 09:10:58 +0100

    feat: test ddpm without RHFlip

--- code/experiments/pipelines.sh
+++ code/experiments/pipelines.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-#python main.py \
-#    --dataset face \
-#    --scheduler ddpm \
-#    --beta_schedule linear \
-#    --image_size 128 \
-#    --num_epochs 500 \
-#    --train_batch_size 64 \
-#    --eval_batch_size 64 \
-#    --wandb_run_name liang_ddpm_linear \
-#    --calculate_fid \
-#    --calculate_is
-
 python main.py \
     --dataset face \
-    --pipeline consistency \
     --scheduler ddpm \
     --beta_schedule linear \
     --image_size 128 \
     --num_epochs 500 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
-    --wandb_run_name liang_RHFlip_consistency_ddpm_linear \
+    --wandb_run_name liang_noRHFlip_ddpm_linear \
     --calculate_fid \
-    --calculate_is \
-    --RHFlip
+    --calculate_is
+
+#python main.py \
+#    --dataset face \
+#    --pipeline consistency \
+#    --scheduler ddpm \
+#    --beta_schedule linear \
+#    --image_size 128 \
+#    --num_epochs 500 \
+#    --train_batch_size 64 \
+#    --eval_batch_size 64 \
+#    --wandb_run_name liang_RHFlip_consistency_ddpm_linear \
+#    --calculate_fid \
+#    --calculate_is \
+#    --RHFlip
 
 
