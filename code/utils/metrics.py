717c04eff42f2a656fcc6877c0ed083300a4a194
Ngene <hplxg@hotmail.com>
2025-04-12 10:29:07 +0100

    fix: test transform_test and FID

--- code/utils/metrics.py
+++ code/utils/metrics.py
@@ -53,9 +53,10 @@ def evaluate(config, epoch, pipeline):
 
 def preprocess_image(image, img_src, device, img_size):
     if img_src == "loaded":
-        # image = (image + 1.0) / 2.0
+        image = (image + 1.0) / 2.0
         # image = F.resize(image, (img_size, img_size))
-        image = torch.tensor(image, device=device)
+        # image = torch.tensor(image).unsqueeze(0)
+        # image = image.permute(0, 3, 1, 2) / 255.0
     elif img_src == "generated":
         image = torch.tensor(image, device=device)
         image = image.permute(0, 3, 1, 2)
