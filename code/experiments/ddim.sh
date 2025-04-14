18a2b86247fffd832cb03fc73bbaaea2b735705a
Syrinx <syrinx_w@163.com>
2025-04-14 21:50:19 +0100

    change batch size for small scale test

--- code/experiments/ddim.sh
+++ code/experiments/ddim.sh
@@ -2,11 +2,12 @@ python main.py \
     --dataset face \
     --pipeline ddim \
     --scheduler ddim \
-    --beta_schedule squaredcos_cap_v2 \
+    --beta_schedule linear \
     --image_size 128 \
-    --num_epochs 500 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
-    --wandb_run_name Ziyu_ddim_cos \
+    --num_epochs 1 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --calculate_fid \
-    --calculate_is
+    --calculate_is \
+    --no_wandb
+    # --wandb_run_name Ziyu_ddim_cos \
