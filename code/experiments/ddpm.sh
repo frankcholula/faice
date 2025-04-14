222b1d65cf1064b5af6a6a5a8a3c01398c94f703
Ngene <hplxg@hotmail.com>
2025-04-14 17:29:06 +0100

    fix: test resize_image function for high resolution resizing

--- code/experiments/ddpm.sh
+++ code/experiments/ddpm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-python main.py \
-    --dataset face \
-    --scheduler ddpm \
-    --beta_schedule linear \
-    --image_size 128 \
-    --num_epochs 500 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
-    --wandb_run_name liang_ddpm_linear_NoAug \
-    --calculate_fid \
-    --calculate_is
+#python main.py \
+#    --dataset face \
+#    --scheduler ddpm \
+#    --beta_schedule linear \
+#    --image_size 128 \
+#    --num_epochs 500 \
+#    --train_batch_size 64 \
+#    --eval_batch_size 64 \
+#    --wandb_run_name liang_ddpm_linear_NoAug \
+#    --calculate_fid \
+#    --calculate_is
 #    --gblur
 #    --RHFlip
 
