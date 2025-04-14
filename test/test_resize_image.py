f2afdbf8b9b710bb87b89c4638d34c980b14700d
Ngene <hplxg@hotmail.com>
2025-04-14 17:08:25 +0100

    add resize image

--- test/test_resize_image.py
+++ test/test_resize_image.py
@@ -1,13 +1,37 @@
+from typing import Tuple
+
 from PIL import Image
-from conf.global_setting import BASE_DIR
 
-image_path = BASE_DIR + "/data/celeba_hq_split/test/1.jpg"
-# Load the 512x512 image
-img = Image.open(image_path)
 
-# Resize to 128x128 with high-quality interpolation
-resized_img = img.resize((128, 128), resample=Image.Resampling.LANCZOS)
+def resize_image(image_data, size: Tuple = (128, 128)):
+    """
+    Resize an image to a specific size.
 
-# Adjust DPI to preserve physical size (e.g., original DPI was 300)
-resized_img.info["dpi"] = (1200, 1200)  # New DPI = (512/128)*300 = 1200
-resized_img.save("resized_image.jpg")
+    Args:
+        image_data:
+        output_path (str): Path to save the resized image.
+        size (tuple): Desired size of the image (width, height).
+
+    Returns:
+        None
+    """
+
+    # Resize to size with high-quality interpolation
+    resized_img = image_data.resize(size, resample=Image.Resampling.LANCZOS)
+
+    # Adjust DPI to preserve physical size (e.g., original DPI was 300)
+    # Get the original DPI and calculate the new DPI
+    original_dpi = img.info.get("dpi", (300, 300))
+    resized_img.info["dpi"] = (
+        original_dpi[0] * img.size[0] / size[0],
+        original_dpi[1] * img.size[1] / size[1],
+    )
+    return resized_img
+
+
+if __name__ == "__main__":
+    image_file = "../datasets/test/1.jpg"
+    # Open the image using Pillow
+    img = Image.open(image_file).convert("RGB")
+    new_img = resize_image(img)
+    new_img.save("resized_image.jpg")
