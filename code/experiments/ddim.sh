7c95a3a8403713aaeae48ac752de2e29180a0870
Syrinx <syrinx_w@163.com>
2025-04-15 18:40:35 +0100

    ddim linear&cos 500

--- code/experiments/ddim.sh
+++ code/experiments/ddim.sh
@@ -4,10 +4,23 @@ python main.py \
     --scheduler ddim \
     --beta_schedule linear \
     --image_size 128 \
-    --num_epochs 200 \
+    --num_epochs 500 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
     --calculate_fid \
     --calculate_is \
-    --wandb_run_name Ziyu_ddim_test_200 \
+    --wandb_run_name Ziyu_ddim_linear \
+
+python main.py \
+    --dataset face \
+    --pipeline ddim \
+    --scheduler ddim \
+    --beta_schedule squaredcos_cap_v2 \
+    --image_size 128 \
+    --num_epochs 500 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --calculate_fid \
+    --calculate_is \
+    --wandb_run_name Ziyu_ddim_cos \
     # --no_wandb
