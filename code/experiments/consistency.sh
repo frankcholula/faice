07af8e919c5637ce45765437c5dd220c7ebf75b4
Ngene <hplxg@hotmail.com>
2025-04-15 18:12:34 +0100

    feat: test unet_resnet_consistency_CMStochastic

--- code/experiments/consistency.sh
+++ code/experiments/consistency.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-python main.py \
-    --dataset face \
-    --pipeline consistency \
-    --scheduler ddpm \
-    --beta_schedule linear \
-    --image_size 128 \
-    --num_epochs 500 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
-    --wandb_run_name liang_consistency_ddpm_linear \
-    --calculate_fid \
-    --calculate_is
-
 #python main.py \
 #    --dataset face \
 #    --pipeline consistency \
-#    --scheduler CMStochastic \
-#    --model unet_resnet \
+#    --scheduler ddpm \
+#    --beta_schedule linear \
 #    --image_size 128 \
 #    --num_epochs 500 \
 #    --train_batch_size 64 \
 #    --eval_batch_size 64 \
-#    --wandb_run_name liang_unet_resnet_consistency_CMStochastic \
+#    --wandb_run_name liang_consistency_ddpm_linear \
 #    --calculate_fid \
 #    --calculate_is
+
+python main.py \
+    --dataset face \
+    --pipeline consistency \
+    --scheduler CMStochastic \
+    --model unet_resnet \
+    --image_size 128 \
+    --num_epochs 500 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name liang_unet_resnet_consistency_CMStochastic \
+    --calculate_fid \
+    --calculate_is
 
 
