4e0e1ca57048ee4dd40f7555c28260b5392a5ac6
Ngene <hplxg@hotmail.com>
2025-04-16 15:37:25 +0100

    feat: test CMStochasticIterativeScheduler

--- code/experiments/consistency.sh
+++ code/experiments/consistency.sh
@@ -20,8 +20,8 @@ python main.py \
     --model unet_resnet \
     --image_size 128 \
     --num_epochs 50 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
+    --train_batch_size 32 \
+    --eval_batch_size 32 \
     --num_train_timesteps 40 \
     --num_inference_steps 1 \
     --wandb_run_name liang_unet_resnet512_consistency_CMStochastic \
