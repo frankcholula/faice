a6374b87d77cc4b51279e73c61bbc16154f9fc44
Syrinx <syrinx_w@163.com>
2025-04-17 01:39:13 +0100

    ddim with new FID

---
+++ code/experiments/ddim.sh
@@ -0,0 +1,26 @@
+python main.py \
+    --dataset face \
+    --pipeline ddpm \
+    --scheduler ddpm \
+    --beta_schedule linear \
+    --image_size 128 \
+    --num_epochs 500 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --calculate_fid \
+    --calculate_is \
+    --wandb_run_name Ziyu_ddim_linear_newFID \
+
+# python main.py \
+#     --dataset face \
+#     --pipeline ddim \
+#     --scheduler ddim \
+#     --beta_schedule squaredcos_cap_v2 \
+#     --image_size 128 \
+#     --num_epochs 500 \
+#     --train_batch_size 64 \
+#     --eval_batch_size 64 \
+#     --calculate_fid \
+#     --calculate_is \
+#     --wandb_run_name Ziyu_ddim_cos_v2 \
+#     # --no_wandb
---
+++ code/pipelines/ddim.py
@@ -0,0 +1,184 @@
+# Deep learning framework
+
+import torch
+import torch.nn.functional as F
+from tqdm.auto import tqdm
+
+# Hugging Face
+from diffusers import DDIMPipeline, DDPMScheduler, DDIMScheduler
+
+# Configuration
+from utils.metrics import calculate_fid_score, calculate_inception_score
+from utils.metrics import evaluate
+from utils.loggers import WandBLogger
+from utils.training import setup_accelerator
+
+
+def train_loop(
+    config,
+    model,
+    noise_scheduler,
+    optimizer,
+    train_dataloader,
+    lr_scheduler,
+    test_dataloader=None,
+):
+    accelerator, repo = setup_accelerator(config)
+    
+    noise_scheduler = DDIMScheduler.from_config(
+    noise_scheduler.config,
+    prediction_type="v_prediction",
+    rescale_betas_zero_snr=True
+)
+
+    # noise_scheduler.config.prediction_type = "v_prediction"
+    # noise_scheduler.config.rescale_betas_zero_snr = True
+    # Use ddpm scheduler for training, set prediction type to "v_prediction", apply rescale_betas_zero_snr
+    # train_noise_scheduler = noise_scheduler
+    # train_noise_scheduler = DDPMScheduler.from_config(noise_scheduler.config, prediction_type="v_prediction", rescale_betas_zero_snr=True)
+    #noise_scheduler(prediction_type="v_prediction", rescale_betas_zero_snr=True)
+    # print("🔍 noise Scheduler Prediction Type:", noise_scheduler.config.prediction_type)
+    # print("🔍 noise Rescale Zero SNR:", noise_scheduler.config.rescale_betas_zero_snr)
+    # print("🔍 train Scheduler Prediction Type:", train_noise_scheduler.config.prediction_type)
+    # print("🔍 train Rescale Zero SNR:", train_noise_scheduler.config.rescale_betas_zero_snr)
+    # print("🔍 noise_scheduler config:")
+    # print(noise_scheduler.config)
+
+    # print("🔍 train_noise_scheduler config:")
+    # print(train_noise_scheduler.config)
+
+    # Initialize wandb
+    WandBLogger.login()
+    wandb_logger = WandBLogger(config, accelerator)
+    wandb_logger.setup(model)
+
+    # Prepare everything
+    # There is no specific order to remember, you just need to unpack the objects in the same order you gave them to the prepare method.
+    if test_dataloader is not None:
+        model, optimizer, train_dataloader, lr_scheduler, test_dataloader = (
+            accelerator.prepare(
+                model, optimizer, train_dataloader, lr_scheduler, test_dataloader
+            )
+        )
+    else:
+        model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+            model, optimizer, train_dataloader, lr_scheduler
+        )
+
+    global_step = 0
+
+    # Now you train the model
+    for epoch in range(config.num_epochs):
+        progress_bar = tqdm(
+            total=len(train_dataloader), disable=not accelerator.is_local_main_process
+        )
+        progress_bar.set_description(f"Epoch {epoch}")
+
+        for step, batch in enumerate(train_dataloader):
+            clean_images = batch["images"]
+            # Sample noise to add to the images
+            noise = torch.randn(clean_images.shape).to(clean_images.device)
+            bs = clean_images.shape[0]
+
+            # Sample a random timestep for each image
+            timesteps = torch.randint(
+                0,
+                noise_scheduler.config.num_train_timesteps,
+                (bs,),
+                device=clean_images.device,
+            ).long()
+
+            # Add noise to the clean images according to the noise magnitude at each timestep
+            # (this is the forward diffusion process)
+            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+            with accelerator.accumulate(model):
+                # Predict the noise residual
+                # noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+                # loss = F.mse_loss(noise_pred, noise)
+                
+                # v = α_t * ε + β_t * x_0, x_0 = clean_images, ε = noise, t = timesteps
+                # v = train_noise_scheduler.get_velocity(clean_images, timesteps, noise)
+                
+                # manually calculate velocity
+                alphas_cumprod = noise_scheduler.alphas_cumprod.to(clean_images.device)
+                sqrt_alpha_prod = alphas_cumprod[timesteps] ** 0.5
+                sqrt_one_minus_alpha_prod = (1 - alphas_cumprod[timesteps]) ** 0.5
+                # reshape to match the shape of clean_images
+                sqrt_alpha_prod = sqrt_alpha_prod.view(-1, 1, 1, 1)
+                sqrt_one_minus_alpha_prod = sqrt_one_minus_alpha_prod.view(-1, 1, 1, 1)
+                v = sqrt_alpha_prod * noise - sqrt_one_minus_alpha_prod * clean_images
+
+                # Predict velocity
+                v_pred = model(noisy_images, timesteps, return_dict=False)[0]
+                loss = F.mse_loss(v_pred, v)
+
+                accelerator.backward(loss)
+
+                accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+            progress_bar.update(1)
+            logs = {
+                "loss": loss.detach().item(),
+                "lr": lr_scheduler.get_last_lr()[0],
+                "step": global_step,
+            }
+            progress_bar.set_postfix(**logs)
+            accelerator.log(logs, step=global_step)
+
+            wandb_logger.log_step(logs, global_step)
+
+            global_step += 1
+
+        # After each epoch you optionally sample some demo images with evaluate() and save the model
+        if accelerator.is_main_process:
+            pipeline = DDIMPipeline(
+                unet=accelerator.unwrap_model(model), scheduler=noise_scheduler
+            )
+
+            generate_samples = (
+                epoch + 1
+            ) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1
+            save_model = (
+                epoch + 1
+            ) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1
+            save_to_wandb = epoch == config.num_epochs - 1
+
+            if generate_samples:
+                evaluate(config, epoch, pipeline)
+            if save_model:
+                if config.push_to_hub:
+                    repo.push_to_hub(commit_message=f"Epoch {epoch}", blocking=True)
+                else:
+                    pipeline.save_pretrained(config.output_dir)
+                    if save_to_wandb:
+                        wandb_logger.save_model()
+
+            progress_bar.close()
+
+    # Now we evaluate the model on the test set
+    if (
+        accelerator.is_main_process
+        and config.calculate_fid
+        and test_dataloader is not None
+    ):
+        pipeline = DDIMPipeline(
+            unet=accelerator.unwrap_model(model), scheduler=noise_scheduler
+        )
+        fid_score = calculate_fid_score(config, pipeline, test_dataloader)
+
+        wandb_logger.log_fid_score(fid_score)
+
+    if (
+        accelerator.is_main_process
+        and config.calculate_is
+        and test_dataloader is not None
+    ):
+        inception_score = calculate_inception_score(
+            config, pipeline, test_dataloader, device=accelerator.device
+        )
+        wandb_logger.log_inception_score(inception_score)
+    wandb_logger.finish()
