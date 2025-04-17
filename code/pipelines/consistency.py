b7cdc46a0243628b6c70e2da685e9b60691148d5
Ngene <hplxg@hotmail.com>
2025-04-17 11:38:23 +0100

    test: the denoise of Consistency model

--- code/pipelines/consistency.py
+++ code/pipelines/consistency.py
@@ -183,9 +183,9 @@ def denoise(model, x_t, sigma, noise_scheduler, **model_kwargs):
             append_dims(x, x_t.ndim)
             for x in get_scalings_for_boundary_condition(noise_scheduler, sigma)
         ]
-    # rescaled_t = 1000 * 0.25 * torch.log(sigma + 1e-44)
+    rescaled_t = 1000 * 0.25 * torch.log(sigma + 1e-44)
     m_input = c_in * x_t
-    model_output = model(m_input, noise_scheduler.timesteps, **model_kwargs)[0]
+    model_output = model(m_input, rescaled_t, **model_kwargs)[0]
     denoised = c_out * model_output + c_skip * x_t
     return model_output, denoised
 
