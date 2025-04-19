1a293dc0ce8b2fdc10594c103fea73bf0cea92d9
Ngene <hplxg@hotmail.com>
2025-04-19 03:40:04 +0100

    test: consistency with scheduler.step

--- code/pipelines/consistency.py
+++ code/pipelines/consistency.py
@@ -79,9 +79,6 @@ def train_loop(
                 #     (bs,),
                 #     dtype=torch.int64,
                 # )
-                noise_scheduler = CMStochasticIterativeScheduler(
-                    num_train_timesteps=config.num_train_timesteps
-                )
                 timesteps_idx = torch.linspace(0, noise_scheduler.config.num_train_timesteps - 1, steps=bs,
                                                dtype=torch.int64)
                 timesteps_idx = torch.flip(timesteps_idx, dims=[0])
@@ -108,26 +105,25 @@ def train_loop(
                     # model_kwargs = {"return_dict": False}
                     # model_output, denoised = denoise(model, noisy_images, sigma, noise_scheduler,
                     #                                  **model_kwargs)
+                    timestops_len = len(init_timesteps)
+                    sigmas_len = len(noise_scheduler.sigmas)
+                    noise_scheduler.set_begin_index()
+                    for i, t in enumerate(init_timesteps):
+                        if step % timestops_len == i:
+                            if noise_scheduler.step_index and noise_scheduler.step_index >= sigmas_len:
+                                noise_scheduler._step_index = 0
+                            scaled_sample = noise_scheduler.scale_model_input(noisy_images, t)
+                            model_output = model(scaled_sample, t, return_dict=False)[0]
 
-                    # noise_scheduler.set_begin_index()
-                    noise_scheduler.set_timesteps(1)
-                    timesteps = noise_scheduler.timesteps
-                    sample = noisy_images
-                    for i, t in enumerate(timesteps):
-                        scaled_sample = noise_scheduler.scale_model_input(sample, t)
-                        model_output = model(scaled_sample, t, return_dict=False)[0]
+                            sample = noise_scheduler.step(model_output, t, noisy_images,
+                                                          generator=torch.manual_seed(0))[0]
 
-                        sample = noise_scheduler.step(model_output, t, noisy_images,
-                                                      generator=torch.manual_seed(0))[0]
-
-                        loss = F.mse_loss(sample, clean_images)
-
+                            loss = F.mse_loss(sample, clean_images)
                 else:
                     noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
                     loss = F.mse_loss(noise_pred, noise)
 
                 accelerator.backward(loss)
-
                 accelerator.clip_grad_norm_(model.parameters(), 1.0)
                 optimizer.step()
                 lr_scheduler.step()
