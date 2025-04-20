c6f468c90f40001b7463da9b5124d3c4bc914c14
Ngene <hplxg@hotmail.com>
2025-04-21 00:17:26 +0100

    test: 10 steps inference for consistency

--- code/experiments/consistency.sh
+++ code/experiments/consistency.sh
@@ -14,37 +14,37 @@
 #    --calculate_is \
 #    --no_confirm
 
-python main.py \
-    --dataset face \
-    --pipeline consistency \
-    --scheduler CMStochastic \
-    --model unet_resnet \
-    --image_size 128 \
-    --num_epochs 100 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
-    --num_train_timesteps 200 \
-    --num_inference_steps 10 \
-    --wandb_run_name liang_unet_resnet512_consistency_CMStochastic_train200 \
-    --calculate_fid \
-    --calculate_is \
-    --no_confirm \
-    --no_wandb
-
-
 #python main.py \
 #    --dataset face \
 #    --pipeline consistency \
 #    --scheduler CMStochastic \
 #    --model unet_resnet \
 #    --image_size 128 \
-#    --num_epochs 500 \
+#    --num_epochs 100 \
 #    --train_batch_size 64 \
 #    --eval_batch_size 64 \
 #    --num_train_timesteps 200 \
-#    --num_inference_steps 1 \
+#    --num_inference_steps 10 \
 #    --wandb_run_name liang_unet_resnet512_consistency_CMStochastic_train200 \
 #    --calculate_fid \
 #    --calculate_is \
-#    --no_confirm
+#    --no_confirm \
+#    --no_wandb
+
+
+python main.py \
+    --dataset face \
+    --pipeline consistency \
+    --scheduler CMStochastic \
+    --model unet_resnet \
+    --image_size 128 \
+    --num_epochs 500 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --num_train_timesteps 200 \
+    --num_inference_steps 2 \
+    --wandb_run_name liang_unet_resnet512_consistency_CMStochastic_train200_inference2 \
+    --calculate_fid \
+    --calculate_is \
+    --no_confirm
 
