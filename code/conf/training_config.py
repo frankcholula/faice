898edd705d47dcc2432a85072450f30916935bec
Ngene <hplxg@hotmail.com>
2025-04-21 07:52:04 +0100

    test: 2 steps 1000 train steps inference for consistency

--- code/conf/training_config.py
+++ code/conf/training_config.py
@@ -74,8 +74,8 @@ class FaceConfig(BaseConfig):
     dataset: str = "face"
     dataset_name: str = "uos-celebahq-256x256"
     num_epochs: int = 1
-    save_image_epochs: int = 20
-    save_model_epochs: int = 20
+    save_image_epochs: int = 50
+    save_model_epochs: int = 50
     train_dir: str = "datasets/celeba_hq_split/train"
     test_dir: str = "datasets/celeba_hq_split/test"
 
