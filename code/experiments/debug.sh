d452eb9479be8f4cd5fd154dd184decfa2c32c81
frankcholula <lu.phrank@gmail.com>
2025-04-22 17:11:43 +0100

    lets try this?

--- code/experiments/debug.sh
+++ code/experiments/debug.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#SBATCH --partition=debug                   #Selecting partition based on GPU type
+#SBATCH --job-name="diffusion_debug"        #Name of Jobs (displayed in squeue)
+#SBATCH --nodes=1                           #No of nodes to run job
+#SBATCH --ntasks-per-node=10                #No of cores to use per node
+#SBATCH --time=00:00:30                     #Maximum time for job to run
+#SBATCH --mem=2G                            #Amount of memory per node
+#SBATCH --gpus=2                            #Selecting 2 x GPUs
+#SBATCH --output=slurm.%N.%j.out            #Output file for stdout (optional)
+#SBATCH --error=slurm.%N.%j.err             #Error file for stderr (optional)
+
 python main.py \
     --dataset face \
     --scheduler ddpm \
