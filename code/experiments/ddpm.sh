ce99701c7155eee501a37459a6bceaac51d55a74
Ngene <hplxg@hotmail.com>
2025-04-22 07:41:45 +0100

    feat: add function for unet_resnet

--- code/experiments/ddpm.sh
+++ code/experiments/ddpm.sh
@@ -29,6 +29,20 @@
 #    --calculate_is \
 #    --no_confirm
 
+#python main.py \
+#    --dataset face \
+#    --scheduler ddpm \
+#    --beta_schedule linear \
+#    --model unet_resnet768 \
+#    --image_size 128 \
+#    --num_epochs 500 \
+#    --train_batch_size 56 \
+#    --eval_batch_size 56 \
+#    --wandb_run_name liang_unet_resnet768_ddpm_linear \
+#    --calculate_fid \
+#    --calculate_is \
+#    --no_confirm
+
 python main.py \
     --dataset face \
     --scheduler ddpm \
@@ -36,9 +50,9 @@ python main.py \
     --model unet_resnet768 \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 56 \
-    --eval_batch_size 56 \
-    --wandb_run_name liang_unet_resnet768_ddpm_linear \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name liang_unet_resnet1024_ddpm_linear \
     --calculate_fid \
     --calculate_is \
     --no_confirm
