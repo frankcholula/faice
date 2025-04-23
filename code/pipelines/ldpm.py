5e6e6cf1ba96cc9de0bb5093432cfd37458f6202
Ngene <hplxg@hotmail.com>
2025-04-23 10:07:11 +0100

    test: ldpm

--- code/pipelines/ldpm.py
+++ code/pipelines/ldpm.py
@@ -87,6 +87,7 @@ def train_loop(
             # noise = torch.randn(clean_images.shape).to(clean_images.device)
             # # Add noise to the clean images according to the noise magnitude at each timestep
             # # (this is the forward diffusion process)
+            latents = vqvae.quantize(latents)
             noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
             with accelerator.accumulate(model):
