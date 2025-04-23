0845ebc54428c162d7098d9f99c3066359b98c06
Ngene <hplxg@hotmail.com>
2025-04-23 10:38:47 +0100

    test: ldpm

--- code/models/vqmodel.py
+++ code/models/vqmodel.py
@@ -14,13 +14,7 @@ def create_vqmodel(config):
         in_channels=3,  # RGB images
         out_channels=3,
         layers_per_block=2,  # how many ResNet layers to use per UNet block
-        block_out_channels=(
-            128,
-            128,
-            256,
-            256,
-            512,
-            512,),
+        block_out_channels=(32, 64, 128),
         act_fn="silu",
         latent_channels=3,
         num_vq_embeddings=512,  # Codebook size
@@ -30,14 +24,8 @@ def create_vqmodel(config):
             "DownEncoderBlock2D",  # a regular ResNet downsampling block
             "DownEncoderBlock2D",
             "DownEncoderBlock2D",
-            "DownEncoderBlock2D",
-            "DownEncoderBlock2D",
-            "DownEncoderBlock2D",
         ),
         up_block_types=(
-            "UpDecoderBlock2D",
-            "UpDecoderBlock2D",
-            "UpDecoderBlock2D",
             "UpDecoderBlock2D",
             "UpDecoderBlock2D",
             "UpDecoderBlock2D",
