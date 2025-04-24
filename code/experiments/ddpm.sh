662e685f9480691008b88797e3ab177361447c32
frankcholula <lu.phrank@gmail.com>
2025-04-25 00:09:08 +0100

    remove useless experiment files.

--- code/experiments/ddpm.sh
+++ code/experiments/ddpm.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-#python main.py \
-#    --dataset face \
-#    --scheduler ddpm \
-#    --beta_schedule linear \
-#    --image_size 128 \
-#    --num_epochs 500 \
-#    --train_batch_size 64 \
-#    --eval_batch_size 64 \
-#    --wandb_run_name liang_ddpm_linear_NoAug \
-#    --calculate_fid \
-#    --calculate_is
-#    --gblur
-#    --RHFlip
-
-
 python main.py \
     --dataset face \
     --scheduler ddpm \
@@ -27,16 +12,3 @@ python main.py \
     --wandb_run_name liang_unet_resnet512_ddpm_linear \
     --calculate_fid \
     --calculate_is
-
-#python main.py \
-#    --dataset face \
-#    --scheduler ddpm \
-#    --beta_schedule linear \
-#    --image_size 128 \
-#    --num_epochs 5 \
-#    --train_batch_size 64 \
-#    --eval_batch_size 64 \
-#    --wandb_run_name liang_ddpm_linear_test \
-#    --calculate_fid \
-#    --calculate_is \
-#    --no_wandb
