445e70d70b4a344ae1b31eae319ffa1cf46ff95b
Ngene <hplxg@hotmail.com>
2025-04-24 18:06:37 +0100

    test: consistency_CMStochastic_train1000_inference100

--- code/experiments/consistency.sh
+++ code/experiments/consistency.sh
@@ -21,8 +21,8 @@ python main.py \
     --model unet_resnet512 \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 128 \
-    --eval_batch_size 128 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
     --num_train_timesteps 1000 \
     --num_inference_steps 60 \
     --wandb_run_name liang_unet_resnet512_consistency_CMStochastic_train1000_inference60 \
