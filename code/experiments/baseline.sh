f4f05141788ce71a8946f64c5c598b061a829fbf
frankcholula <lu.phrank@gmail.com>
2025-04-25 00:07:24 +0100

    added baseline script.

---
+++ code/experiments/baseline.sh
@@ -0,0 +1,14 @@
+python main.py \
+    --dataset face \
+    --scheduler ddpm \
+    --beta_schedule linear \
+    --model unet \
+    --unet_variant ddpm \
+    --image_size 128 \
+    --num_epochs 500 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name baseline_ddpm \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
