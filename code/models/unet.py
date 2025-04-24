f0f2380de2a114189626583a670b7ea023dec4a8
frankcholula <lu.phrank@gmail.com>
2025-04-24 23:18:58 +0100

    fixed everything.

--- code/models/unet.py
+++ code/models/unet.py
@@ -2,7 +2,9 @@ from diffusers import UNet2DModel
 
 
 class BaseUNet(UNet2DModel):
-    """Baseline model given to us. DO NOT TWEAK."""
+    """Baseline model given. Don't tweak this.
+    This is technically wrong because it's built for 256 x 256 images.
+    """
 
     def __init__(self, config):
         super().__init__(
@@ -10,29 +12,28 @@ class BaseUNet(UNet2DModel):
             in_channels=3,
             out_channels=3,
             layers_per_block=2,
-            attention_head_dim=8,
             block_out_channels=(128, 128, 256, 256, 512, 512),
             down_block_types=(
-                "DownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
-                "AttnDownBlock2D",
-                "DownBlock2D",
+                "DownBlock2D",  # 256 -> 128
+                "DownBlock2D",  # 128 -> 64
+                "DownBlock2D",  # 64 -> 32
+                "DownBlock2D",  # 32 -> 16
+                "AttnDownBlock2D",  # 16 -> 8
+                "DownBlock2D",  # 8 -> 4
             ),
             up_block_types=(
-                "UpBlock2D",
-                "AttnUpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
+                "UpBlock2D",  # 4 -> 8
+                "AttnUpBlock2D",  # 8 -> 16
+                "UpBlock2D",  # 16 -> 32
+                "UpBlock2D",  # 32 -> 64
+                "UpBlock2D",  # 64 -> 128
+                "UpBlock2D",  # 128 -> 256
             ),
         )
 
 
-class UNetAttentionHeads(UNet2DModel):
-    """Change the attention resolution to 16 x 16 since we are starting with 128 x 128 images."""
+class DDPMUNet(UNet2DModel):
+    """This class mirrors the DDPM paper."""
 
     def __init__(self, config):
         super().__init__(
@@ -40,29 +41,30 @@ class UNetAttentionHeads(UNet2DModel):
             in_channels=3,
             out_channels=3,
             layers_per_block=2,
-            attention_head_dim=64,
+            attention_head_dim=512,  # 512 for single head attention at the 16 x 16 resolution.
+            time_embedding_type="positional",
             block_out_channels=(128, 128, 256, 256, 512, 512),
             down_block_types=(
-                "DownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
-                "AttnDownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
+                "DownBlock2D",  # 128 -> 64
+                "DownBlock2D",  # 64 -> 32
+                "DownBlock2D",  # 32 -> 16
+                "AttnDownBlock2D",  # 16 -> 8
+                "DownBlock2D",  # 8 -> 4
+                "DownBlock2D",  # 4 -> 2
             ),
             up_block_types=(
-                "UpBlock2D",
-                "UpBlock2D",
-                "AttnUpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
+                "UpBlock2D",  # 2 -> 4
+                "UpBlock2D",  # 4 -> 8
+                "AttnUpBlock2D",  # 8 -> 16
+                "UpBlock2D",  # 16 -> 32
+                "UpBlock2D",  # 32 -> 64
+                "UpBlock2D",  # 64 -> 128
             ),
         )
 
 
-class UNetMultiResAttentionHeads(UNet2DModel):
-    """Adding attention at 32 x 32, 16 x 16, and 8 x 8 resolutions."""
+class ADMUNet(UNet2DModel):
+    """This is the model used in the AMDM paper. We should run some ablations using this class."""
 
     def __init__(self, config):
         super().__init__(
@@ -93,15 +95,12 @@ class UNetMultiResAttentionHeads(UNet2DModel):
 
 ARCHITECTURES = {
     "base": BaseUNet,
-    "attention_heads": UNetAttentionHeads,
-    "multi_res": UNetMultiResAttentionHeads,
+    "ddpm": DDPMUNet,
+    "adm": ADMUNet,
 }
 
 
 def create_unet(config):
-    """
-    config.unet_variant should be one of: "base", "attention_heads", or "multi_res"
-    """
     try:
         cls = ARCHITECTURES[config.unet_variant]
     except KeyError:
