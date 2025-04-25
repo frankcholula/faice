7ca4a72d1edf2a3cc09cb53b7d555a21dabf2690
frankcholula <lu.phrank@gmail.com>
2025-04-25 10:38:08 +0100

    reset batch size.

--- code/experiments/task1/adm.sh
+++ code/experiments/task1/adm.sh
@@ -8,8 +8,8 @@ python main.py \
     --unet_variant adm \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 32 \
-    --eval_batch_size 32 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
     --wandb_run_name task1_adm \
     --calculate_fid \
     --calculate_is \
