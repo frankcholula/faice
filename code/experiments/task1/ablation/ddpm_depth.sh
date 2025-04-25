fdc894fdd2f5d600358968361925b62d5c809058
frankcholula <lu.phrank@gmail.com>
2025-04-25 18:53:12 +0100

    reduce batch size due to depth.

--- code/experiments/task1/ablation/ddpm_depth.sh
+++ code/experiments/task1/ablation/ddpm_depth.sh
@@ -9,8 +9,8 @@ python main.py \
     --layers_per_block 4 \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 28 \
-    --eval_batch_size 28 \
+    --train_batch_size 14 \
+    --eval_batch_size 14 \
     --wandb_run_name task1_ddpm_depth_ablation_otter \
     --calculate_fid \
     --calculate_is \
