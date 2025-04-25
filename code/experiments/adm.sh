34da4d46e864f1468fc707378ff6204435d17f81
frankcholula <lu.phrank@gmail.com>
2025-04-26 00:32:55 +0100

    standardize all batch size for ablation.

--- code/experiments/adm.sh
+++ code/experiments/adm.sh
@@ -9,8 +9,8 @@ python main.py \
     --unet_variant adm \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 32 \
-    --eval_batch_size 32 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --wandb_run_name task1_adm \
     --calculate_fid \
     --calculate_is \
