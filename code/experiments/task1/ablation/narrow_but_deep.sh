b2e52ea01ec516aa8cc36ed32df85102764995eb
frankcholula <lu.phrank@gmail.com>
2025-04-25 19:03:16 +0100

    some renaming.

--- code/experiments/task1/ablation/narrow_but_deep.sh
+++ code/experiments/task1/ablation/narrow_but_deep.sh
@@ -12,7 +12,7 @@ python main.py \
     --num_epochs 500 \
     --train_batch_size 28 \
     --eval_batch_size 28 \
-    --wandb_run_name task1_ddpm_depth_ablation_otter \
+    --wandb_run_name task1_ddpm_width_depth_ablation_otter \
     --calculate_fid \
     --calculate_is \
     --verbose
