e5568aad296ddb023d6f4f9eaf6dcf8b2a5b49b6
frankcholula <lu.phrank@gmail.com>
2025-04-26 01:24:08 +0100

    changed some names.

--- code/experiments/task1/ablation/ddpm_resnet_sampling.sh
+++ code/experiments/task1/ablation/ddpm_resnet_sampling.sh
@@ -16,7 +16,7 @@ python main.py \
     --num_epochs 500 \
     --train_batch_size 24 \
     --eval_batch_size 24 \
-    --wandb_run_name task1_ddpm_heads_ablation \
+    --wandb_run_name task1_ddpm_resnet_sampling_ablation \
     --calculate_fid \
     --calculate_is \
     --verbose
