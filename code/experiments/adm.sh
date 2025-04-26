dcd12bc19e24702a641866f053623ef0a79fb873
frankcholula <lu.phrank@gmail.com>
2025-04-26 02:28:24 +0100

    changed to 24.

--- code/experiments/adm.sh
+++ code/experiments/adm.sh
@@ -9,8 +9,8 @@ python main.py \
     --unet_variant adm \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 16 \
-    --eval_batch_size 16 \
+    --train_batch_size 24 \
+    --eval_batch_size 24 \
     --wandb_run_name task1_adm \
     --calculate_fid \
     --calculate_is \
