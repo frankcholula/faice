63fbd9e154b8c948483324a29dbb2fec730e060e
Ngene <hplxg@hotmail.com>
2025-04-27 11:09:51 +0100

    test: vqvae

--- code/experiments/vqvae.sh
+++ code/experiments/vqvae.sh
@@ -10,8 +10,8 @@ python main.py \
     --pipeline vqvae \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
+    --train_batch_size 60 \
+    --eval_batch_size 60 \
     --wandb_run_name liang_vqvae \
     --no_confirm \
     --no_wandb
