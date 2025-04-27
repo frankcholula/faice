e026d1fe4c48fc68265bcfa4301478f746b39095
Ngene <hplxg@hotmail.com>
2025-04-27 21:13:59 +0100

    test: vae

--- code/models/vae.py
+++ code/models/vae.py
@@ -15,7 +15,8 @@ def create_vae(config):
         out_channels=3,  # the number of output channels
         layers_per_block=2,  # how many ResNet layers to use per UNet block
         latent_channels=16,
-        block_out_channels=(128, 256, 512),
+        # block_out_channels=(128, 256, 512),
+        block_out_channels=(128, 64, 32),
         down_block_types=(
             "DownEncoderBlock2D",
             "DownEncoderBlock2D",
