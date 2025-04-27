9a3d317558f45a5f49da4483913949d119c56ad0
Kaan Emre Sanal <kemresanal@gmail.com>
2025-04-27 18:53:58 +0300

    Added ablation experiments for task 4 (hyperparameter tuning)

---
+++ code/experiments/task4/ablation/cmstochastic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python main.py \
+    --dataset face \
+    --scheduler ddpm \
+    --beta_schedule linear \
+    --model unet \
+    --unet_variant ddpm \
+    --image_size 128 \
+    --num_epochs 500 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name task4_ablation_baseline \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
