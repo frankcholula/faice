a4cdc2d2a1c97de4a5878b747625440906f58878
Ngene <hplxg@hotmail.com>
2025-04-27 12:14:20 +0100

    test: vqvae

--- code/pipelines/vqvae_train.py
+++ code/pipelines/vqvae_train.py
@@ -118,7 +118,8 @@ def train_loop(
             and config.calculate_fid
             and test_dataloader is not None
     ):
-        vqvae_inference(config.output_dir, config, test_dataloader)
+        model_path = config.output_dir + '/model_vqvae.pth'
+        vqvae_inference(model_path, config, test_dataloader)
 
     wandb_logger.finish()
 
