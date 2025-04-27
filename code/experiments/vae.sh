7cb99cfd74045fd2cffe81ac36f7d70e69c01f94
Ngene <hplxg@hotmail.com>
2025-04-27 19:30:40 +0100

    test: vae

--- code/experiments/vae.sh
+++ code/experiments/vae.sh
@@ -8,7 +8,7 @@ python main.py \
     --model vae \
     --pipeline vae \
     --image_size 128 \
-    --num_epochs 20 \
+    --num_epochs 5 \
     --train_batch_size 60 \
     --eval_batch_size 60 \
     --wandb_run_name liang_vae \
