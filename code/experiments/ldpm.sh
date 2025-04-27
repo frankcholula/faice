0ec19b746c9d50419ae151f3b75cdb97aeb0255f
Ngene <hplxg@hotmail.com>
2025-04-27 10:24:15 +0100

    test: ldpm

--- code/experiments/ldpm.sh
+++ code/experiments/ldpm.sh
@@ -11,8 +11,8 @@ python main.py \
     --beta_schedule linear \
     --image_size 128 \
     --num_epochs 500 \
-    --train_batch_size 256 \
-    --eval_batch_size 256 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
     --wandb_run_name liang_ldpm_ddim_linear \
     --calculate_fid \
     --calculate_is \
