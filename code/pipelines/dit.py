59b4a94e5b360230852548d319ca3052aeaeee1e
Ngene <hplxg@hotmail.com>
2025-04-28 15:49:18 +0100

    test: dit

--- code/pipelines/dit.py
+++ code/pipelines/dit.py
@@ -66,13 +66,13 @@ def train_loop(
             # Convert the name in image_names to int number
             image_names = image_names.astype(int)
             image_names = torch.tensor(image_names, dtype=torch.int)
-            bs = clean_images.shape[0]
-            num_classes = 2700
-            # Convert the name in image_names to int number
-            class_embedding = nn.Embedding(num_classes, bs)
-
-            class_embedding_vector = class_embedding(image_names)
-            map_ids = class_embedding_vector
+            # bs = clean_images.shape[0]
+            # num_classes = 2700
+            # # Convert the name in image_names to int number
+            # class_embedding = nn.Embedding(num_classes, bs)
+            #
+            # class_embedding_vector = class_embedding(image_names)
+            map_ids = image_names
 
             vae.to(clean_images.device)
 
