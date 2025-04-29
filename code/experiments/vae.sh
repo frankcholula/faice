8a9ddf38eb42b05cade048d7d26fae364dc837db
Ngene <hplxg@hotmail.com>
2025-04-29 22:47:40 +0100

    feat: change evaluate for vae

--- code/experiments/vae.sh
+++ code/experiments/vae.sh
@@ -9,8 +9,8 @@ python main.py \
     --pipeline vae \
     --image_size 128 \
     --num_epochs 1 \
-    --train_batch_size 28 \
-    --eval_batch_size 28 \
+    --train_batch_size 56 \
+    --eval_batch_size 56 \
     --wandb_run_name liang_vae_batch_size_28 \
     --calculate_fid \
     --no_confirm \
