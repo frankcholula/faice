1650022202348ce1996d289326157d163d1621d0
frankcholula <lu.phrank@gmail.com>
2025-04-29 20:24:25 +0100

    lets get this working.

--- code/conf/training_config.py
+++ code/conf/training_config.py
@@ -76,7 +76,7 @@ class FaceConfig(BaseConfig):
     dataset: str = "face"
     dataset_name: str = "uos-celebahq-256x256"
     num_epochs: int = 1
-    save_image_epochs: int = 50
+    save_image_epochs: int = 10
     save_model_epochs: int = 50
     train_dir: str = "datasets/celeba_hq_split/train"
     test_dir: str = "datasets/celeba_hq_split/test"
