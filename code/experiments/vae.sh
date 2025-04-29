f41195faf95316954c4ef9f89d75b7f5bd68ec62
Ngene <hplxg@hotmail.com>
2025-04-29 20:40:22 +0100

    test: vae GPU memmory

--- code/experiments/vae.sh
+++ code/experiments/vae.sh
@@ -9,8 +9,8 @@ python main.py \
     --pipeline vae \
     --image_size 128 \
     --num_epochs 1 \
-    --train_batch_size 16 \
-    --eval_batch_size 16 \
+    --train_batch_size 28 \
+    --eval_batch_size 28 \
     --wandb_run_name liang_vae_batch_size_40 \
     --calculate_fid \
     --no_confirm \
