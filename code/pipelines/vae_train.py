4492f6390d342424163ee7f722797850853ef4cb
Ngene <hplxg@hotmail.com>
2025-04-29 13:09:49 +0100

    test: vae

--- code/pipelines/vae_train.py
+++ code/pipelines/vae_train.py
@@ -213,7 +213,7 @@ def evaluate(config, epoch, decoded):
     generated_images = (decoded / 2 + 0.5).clamp(0, 1)
     # Make a grid out of the images
     # Convert the image size (b, c, h, w) to (b, w, h)
-    generated_images = generated_images.permute(0, 2, 3, 1)
+    generated_images = generated_images.permute(0, 3, 2, 1)
     generated_images = generated_images[:16]
     image_grid = make_grid(generated_images, rows=4, cols=4)
 
