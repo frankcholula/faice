8f8bfee7fc9dd717ac88b92b19b6a2ec6e695493
Ngene <hplxg@hotmail.com>
2025-04-29 23:24:06 +0100

    test: vqvae 28 batch size

--- code/experiments/vqvae.sh
+++ code/experiments/vqvae.sh
@@ -9,7 +9,7 @@ python main.py \
     --model vqvae \
     --pipeline vqvae \
     --image_size 128 \
-    --num_epochs 1 \
+    --num_epochs 500 \
     --train_batch_size 28 \
     --eval_batch_size 28 \
     --wandb_run_name liang_vqvae_batch_size_28 \
