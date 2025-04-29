da02608ad157df121e8699ef92b3b983c48178b8
Ngene <hplxg@hotmail.com>
2025-04-29 18:36:34 +0100

    train: vae GPU memory

--- code/experiments/vae.sh
+++ code/experiments/vae.sh
@@ -9,9 +9,9 @@ python main.py \
     --pipeline vae \
     --image_size 128 \
     --num_epochs 1 \
-    --train_batch_size 40 \
-    --eval_batch_size 40 \
-    --wandb_run_name liang_vae_batch_size_40 \
+    --train_batch_size 32 \
+    --eval_batch_size 32 \
+    --wandb_run_name liang_vae_batch_size_32 \
     --calculate_fid \
     --no_confirm \
     --no_wandb
