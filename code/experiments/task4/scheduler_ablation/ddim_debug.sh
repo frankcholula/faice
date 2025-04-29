9c55f099b2663acf5f8974cb07305bf76cfaaf07
frankcholula <lu.phrank@gmail.com>
2025-04-29 19:21:48 +0100

    lets debug this.

--- code/experiments/task4/scheduler_ablation/ddim_debug.sh
+++ code/experiments/task4/scheduler_ablation/ddim_debug.sh
@@ -10,13 +10,13 @@ python main.py \
     --model unet \
     --unet_variant adm \
     --attention_head_dim 64 \
-    --prediction_type v_prediction \
+    --prediction_type epsilon \
     --rescale_betas_zero_snr \
     --image_size 128 \
-    --num_epochs 500 \
+    --num_epochs 50 \
     --num_train_timesteps 4000 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --wandb_run_name task4_ddim_100 \
     --calculate_fid \
     --calculate_is \
