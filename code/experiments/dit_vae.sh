1afe2b5283892a26a59dcbe38a790bf5d75d6b47
Ngene <hplxg@hotmail.com>
2025-04-30 20:40:59 +0100

    test: dit_vae with transformer_2d

--- code/experiments/dit_vae.sh
+++ code/experiments/dit_vae.sh
@@ -3,9 +3,28 @@
 wandb sync --clean-old-hours 1
 wandb artifact cache cleanup 0GB --remove-temp
 
+#python main.py \
+#    --dataset face \
+#    --model dit_transformer \
+#    --pipeline dit_vae \
+#    --scheduler ddim \
+#    --beta_schedule scaled_linear \
+#    --image_size 128 \
+#    --num_epochs 20 \
+#    --num_train_timesteps 1000 \
+#    --num_inference_steps 1000 \
+#    --train_batch_size 40 \
+#    --eval_batch_size 40 \
+#    --wandb_run_name liang_dit_vae_ddpm_linear_batch_size_40_test \
+#    --calculate_fid \
+#    --calculate_is \
+#    --no_confirm \
+#    --no_wandb
+
+
 python main.py \
     --dataset face \
-    --model dit_transformer \
+    --model transformer_2d \
     --pipeline dit_vae \
     --scheduler ddim \
     --beta_schedule scaled_linear \
@@ -20,4 +39,3 @@ python main.py \
     --calculate_is \
     --no_confirm \
     --no_wandb
-
