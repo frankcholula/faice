c0c03721fb18fd80495511748e9b8794d05845cc
Ngene <hplxg@hotmail.com>
2025-04-30 16:04:03 +0100

    test: dit gpu memory

--- code/experiments/vqvae.sh
+++ code/experiments/vqvae.sh
@@ -10,8 +10,8 @@ python main.py \
     --pipeline vqvae \
     --image_size 128 \
     --num_epochs 1 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
+    --train_batch_size 96 \
+    --eval_batch_size 96 \
     --wandb_run_name liang_vqvae_batch_size_56 \
     --calculate_fid \
     --no_confirm \
