2d84d9e417028b59b5115c7cd79d3b51ab67473e
Ngene <hplxg@hotmail.com>
2025-04-30 22:55:01 +0100

    feat: add test_vae

--- code/test/test_vae.py
+++ code/test/test_vae.py
@@ -26,7 +26,7 @@ def vae_inference():
     vae = vae.to(config.device)
     vae.load_state_dict(checkpoint['model_state_dict'])
 
-    noise = torch.randn(16, 16, 16, 16).to(config.device)
+    noise = torch.randn(16, 16, 32, 32).to(config.device)
     decoded = vae.decode(noise)[0]
 
     generated_images = (decoded / 2 + 0.5).clamp(0, 1)
