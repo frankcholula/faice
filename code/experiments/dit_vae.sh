5c2ca68378cf7feae23149862263ca0771069104
Ngene <hplxg@hotmail.com>
2025-04-30 11:09:39 +0100

    test: dit_vae

--- code/experiments/dit_vae.sh
+++ code/experiments/dit_vae.sh
@@ -13,8 +13,8 @@ python main.py \
     --num_epochs 1 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 16 \
-    --eval_batch_size 16 \
+    --train_batch_size 10 \
+    --eval_batch_size 10 \
     --wandb_run_name liang_dit_vae_ddpm_linear_batch_size_10_test \
     --calculate_fid \
     --calculate_is \
