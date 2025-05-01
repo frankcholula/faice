bef788e1b20c8d5e144c9491f17716b4927748fc
Ngene <hplxg@hotmail.com>
2025-05-01 08:21:26 +0100

    test: transformer_2d

--- code/experiments/dit.sh
+++ code/experiments/dit.sh
@@ -5,7 +5,7 @@ wandb artifact cache cleanup 0GB --remove-temp
 
 python main.py \
     --dataset face \
-    --model transformer_2d_xformers_fast \
+    --model transformer_2d \
     --pipeline dit \
     --scheduler ddpm \
     --beta_schedule linear \
@@ -15,10 +15,28 @@ python main.py \
     --num_inference_steps 1000 \
     --train_batch_size 160 \
     --eval_batch_size 160 \
-    --wandb_run_name liang_transformer_2d_xformers_fast_ddpm_linear \
+    --wandb_run_name liang_transformer_2d_ddpm_linear \
     --calculate_fid \
     --calculate_is \
-    --no_confirm
+    --no_confirm \
+    --no_wandb
+
+#python main.py \
+#    --dataset face \
+#    --model transformer_2d_xformers_fast \
+#    --pipeline dit \
+#    --scheduler ddpm \
+#    --beta_schedule linear \
+#    --image_size 128 \
+#    --num_epochs 500 \
+#    --num_train_timesteps 1000 \
+#    --num_inference_steps 1000 \
+#    --train_batch_size 160 \
+#    --eval_batch_size 160 \
+#    --wandb_run_name liang_transformer_2d_xformers_fast_ddpm_linear \
+#    --calculate_fid \
+#    --calculate_is \
+#    --no_confirm
 #    --no_wandb
 
 #python main.py \
