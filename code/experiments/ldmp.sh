1587b00d6671c684eac127e6deaca0919c9af8c0
Ngene <hplxg@hotmail.com>
2025-05-01 11:37:34 +0100

    train: ldmp with pretrain vqvae

--- code/experiments/ldmp.sh
+++ code/experiments/ldmp.sh
@@ -10,13 +10,13 @@ python main.py \
     --scheduler ddim \
     --beta_schedule scaled_linear \
     --image_size 128 \
-    --num_epochs 100 \
+    --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
     --train_batch_size 96 \
     --eval_batch_size 96 \
-    --wandb_run_name liang_ldmp_ddim_linear_pretrain_vqvae \
+    --wandb_run_name liang_ldmp_ddim_scaled_linear_pretrain_vqvae_bs96 \
     --calculate_fid \
     --calculate_is \
-    --no_confirm \
-    --no_wandb
+    --no_confirm
+#    --no_wandb
