65331d06468e55c1a588285bed6fe77d9605d410
Ngene <hplxg@hotmail.com>
2025-05-01 12:35:34 +0100

    feat: add sample method for vae

--- code/experiments/dit_vae.sh
+++ code/experiments/dit_vae.sh
@@ -50,8 +50,8 @@ python main.py \
     --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 24 \
-    --eval_batch_size 24 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --wandb_run_name liang_transformer_2d_vae_ddim_linear_pretrain_vae \
     --calculate_fid \
     --calculate_is \
