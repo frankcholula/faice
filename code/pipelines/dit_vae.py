9802d7d237e0084441a444c3c9d3e357ab18b7ec
Ngene <hplxg@hotmail.com>
2025-05-01 15:51:18 +0100

    test: dit_vae

--- code/pipelines/dit_vae.py
+++ code/pipelines/dit_vae.py
@@ -14,6 +14,7 @@ from utils.metrics import evaluate
 from utils.loggers import WandBLogger
 from utils.training import setup_accelerator
 from models.vae import create_vae
+from pipelines.dit import name_to_label
 
 selected_pipeline = DiTPipeline
 vae_path = "runs/vae-vae-ddpm-face-500/checkpoints/model_vae.pth"
@@ -79,13 +80,13 @@ def train_loop(
             image_names = batch["image_names"]
             bs = clean_images.shape[0]
 
-            image_names = [img_name[0] for img_name in image_names]
+            image_labels = [name_to_label(img_name) for img_name in image_names]
 
-            image_names = np.array(image_names)
+            image_labels = np.array(image_labels)
             # Convert the name in image_names to int number
-            image_names = image_names.astype(int)
-            map_ids = torch.tensor(image_names, dtype=torch.int)
-            map_ids = map_ids.to(clean_images.device)
+            image_labels = image_labels.astype(int)
+            map_ids = torch.tensor(image_labels, dtype=torch.int)
+            map_ids = map_ids.to(device)
 
             vae.to(device)
 
