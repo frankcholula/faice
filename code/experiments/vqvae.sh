a56f7717902313c356aee1a98d823da49b539c23
Ngene <hplxg@hotmail.com>
2025-05-01 08:29:02 +0100

    test: transformer_2d

--- code/experiments/vqvae.sh
+++ code/experiments/vqvae.sh
@@ -9,7 +9,7 @@ python main.py \
     --model vqvae \
     --pipeline vqvae \
     --image_size 128 \
-    --num_epochs 400 \
+    --num_epochs 500 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
     --wandb_run_name liang_vqvae_batch_size_64_latent_channels_32 \
