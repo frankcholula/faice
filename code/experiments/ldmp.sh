0433b66ca8794cba7d17bf220b802d6d4e3a7212
Ngene <hplxg@hotmail.com>
2025-05-01 10:54:46 +0100

    test: ldmp with pretrain vqvae

--- code/experiments/ldmp.sh
+++ code/experiments/ldmp.sh
@@ -10,7 +10,7 @@ python main.py \
     --scheduler ddim \
     --beta_schedule scaled_linear \
     --image_size 128 \
-    --num_epochs 1 \
+    --num_epochs 100 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
     --train_batch_size 96 \
