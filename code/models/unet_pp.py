b02528dcc5e10ef1248c93bd02585fc6b2ef9a8b
Ngene <hplxg@hotmail.com>
2025-05-01 22:07:21 +0100

    feat: add unet_pp; test loss function for dit vae

---
+++ code/models/unet_pp.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+"""
+@Time : 01/05/2025 21:49
+@Author : xiaoguangliang
+@File : unet_pp.py
+@Project : code
+"""
+import segmentation_models_pytorch as smp
+
+
+def unet_pp(
+        classes=5,
+        in_channels=3,
+):
+    """U-Net++
+
+    Args:
+        encoder_name (_type_): _description_
+        encoder_weights (_type_): _description_
+        classes (_type_): _description_
+    """
+    return smp.UnetPlusPlus(
+        classes=classes,
+        in_channels=in_channels,
+    )
