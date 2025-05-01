70acd919088494afcacd16d6f77d8a39e25bfee5
frankcholula <lu.phrank@gmail.com>
2025-05-01 14:46:19 +0100

    cleaned up ablation exp code.

--- code/experiments/task1/adm.sh
+++ code/experiments/task1/adm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This is after all the optimizations for ADM. 
+
 python main.py \
     --dataset face \
     --scheduler ddpm \
@@ -12,7 +13,7 @@ python main.py \
     --num_epochs 500 \
     --train_batch_size 24 \
     --eval_batch_size 24 \
-    --wandb_run_name task1_adm \
+    --wandb_run_name final \
     --calculate_fid \
     --calculate_is \
     --verbose
