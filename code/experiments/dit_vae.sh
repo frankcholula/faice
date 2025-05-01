e34a2c0a88eebd14d8a157e330d70a153b95f5a8
Ngene <hplxg@hotmail.com>
2025-05-01 12:33:47 +0100

    feat: add sample method for vae

--- code/experiments/dit_vae.sh
+++ code/experiments/dit_vae.sh
@@ -47,7 +47,7 @@ python main.py \
     --scheduler ddim \
     --beta_schedule linear \
     --image_size 128 \
-    --num_epochs 300 \
+    --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
     --train_batch_size 32 \
