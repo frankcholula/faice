513b01ae643031296f40fa4c3dbc0015de66e677
Ngene <hplxg@hotmail.com>
2025-05-01 19:32:07 +0100

    test: dit  image_size: int = 256

--- code/experiments/dit_vae.sh
+++ code/experiments/dit_vae.sh
@@ -68,8 +68,8 @@ python main.py \
     --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 4 \
-    --eval_batch_size 4 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --wandb_run_name liang_create_transformer_2d_vae_ddim_linear_vae16 \
     --calculate_fid \
     --calculate_is \
