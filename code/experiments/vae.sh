fa0adb203d70cc054f570c74a9b0e8784991fdc2
Ngene <hplxg@hotmail.com>
2025-05-01 18:19:39 +0100

    train: vae_xl_latent_channels4_batch_size_120

--- code/experiments/vae.sh
+++ code/experiments/vae.sh
@@ -21,10 +21,10 @@ python main.py \
     --model vae_xl \
     --pipeline vae \
     --image_size 128 \
-    --num_epochs 1 \
+    --num_epochs 500 \
     --train_batch_size 112 \
     --eval_batch_size 112 \
     --wandb_run_name liang_vae_xl_latent_channels4_batch_size_120 \
     --calculate_fid \
     --no_confirm \
-    --no_wandb
+#    --no_wandb
