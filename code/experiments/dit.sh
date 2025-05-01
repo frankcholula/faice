356e85e54ceee76084aaca67824226e4cd37f0e5
Ngene <hplxg@hotmail.com>
2025-05-01 09:04:50 +0100

    test: transformer_2d_xformers_fast

--- code/experiments/dit.sh
+++ code/experiments/dit.sh
@@ -31,13 +31,13 @@ python main.py \
     --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 96 \
-    --eval_batch_size 96 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
     --wandb_run_name liang_transformer_2d_xformers_fast_ddpm_linear_layers12 \
     --calculate_fid \
     --calculate_is \
-    --no_confirm
-#    --no_wandb
+    --no_confirm \
+    --no_wandb
 
 #python main.py \
 #    --dataset face \
