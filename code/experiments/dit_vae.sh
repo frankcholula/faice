0c6518d6a251f4eafa4da8fc4c6890b311e6fde6
Ngene <hplxg@hotmail.com>
2025-05-02 17:57:24 +0100

    test: DPMSolverMultistepScheduler

--- code/experiments/dit_vae.sh
+++ code/experiments/dit_vae.sh
@@ -104,8 +104,8 @@ python main.py \
     --num_epochs 1 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 8 \
-    --eval_batch_size 8 \
+    --train_batch_size 164 \
+    --eval_batch_size 164 \
     --wandb_run_name liang_dit_transformer_dpmsolvermultistep_linear_vae16 \
     --calculate_fid \
     --calculate_is \
