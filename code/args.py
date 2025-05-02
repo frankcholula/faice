e63c394eef0e524dcc7a0791d17142ae12976366
Ngene <hplxg@hotmail.com>
2025-05-02 15:02:15 +0100

    feat: add solver_order = 3 for dit_vae

--- code/args.py
+++ code/args.py
@@ -32,7 +32,7 @@ def create_scheduler(
         )
     elif scheduler.lower() == "dpmsolvermultistep":
         return DPMSolverMultistepScheduler(
-            num_train_timesteps=num_train_timesteps, beta_schedule=beta_schedule
+            num_train_timesteps=num_train_timesteps, beta_schedule=beta_schedule, solver_order=3
         )
     elif scheduler.lower() not in ["ddpm", "ddim", "pndm", "cmstochastic"]:
         raise ValueError(f"Scheduler type '{scheduler}' is not supported.")
