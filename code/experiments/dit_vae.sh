13713410953f74d26f0047e4dd1f218acab60a59
Ngene <hplxg@hotmail.com>
2025-05-02 23:32:54 +0100

    test: dit_transformer_ddim_linear_vae4

--- code/experiments/dit_vae.sh
+++ code/experiments/dit_vae.sh
@@ -140,12 +140,12 @@ python main.py \
     --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 82 \
-    --eval_batch_size 82 \
+    --train_batch_size 168 \
+    --eval_batch_size 168 \
     --wandb_run_name liang_dit_transformer_ddim_linear_vae4_bs82 \
     --calculate_fid \
     --calculate_is \
-    --no_confirm
-#    --no_wandb
+    --no_confirm \
+    --no_wandb
 
 
