9928fdf47544c3c1093b60b68d55f9a8fe6c5d3a
Ngene <hplxg@hotmail.com>
2025-05-03 09:36:53 +0100

    test: ema in dit model

--- code/pipelines/dit_vae.py
+++ code/pipelines/dit_vae.py
@@ -188,7 +188,7 @@ def train_loop(
     ema = deepcopy(model).to(device)  # Create an EMA of the model for use after training
     requires_grad(ema, False)
     # Prepare models for training:
-    update_ema(ema, model.module, decay=0)  # Ensure EMA is initialized with synced weights
+    update_ema(ema, model.modules, decay=0)  # Ensure EMA is initialized with synced weights
     model.train()  # important! This enables embedding dropout for classifier-free guidance
     ema.eval()  # EMA model should always be in eval mode
 
