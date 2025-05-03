83d6c939660deb91da6ab2bec5ac2ad54da61b56
Ngene <hplxg@hotmail.com>
2025-05-03 15:47:36 +0100

    test: latent_unet_xl_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.1

--- code/experiments/ldmp.sh
+++ code/experiments/ldmp.sh
@@ -76,9 +76,27 @@ wandb artifact cache cleanup 0GB --remove-temp
 #    --no_confirm
 #    --no_wandb
 
+#python main.py \
+#    --dataset face \
+#    --model latent_unet \
+#    --pipeline ldmp \
+#    --scheduler ddim \
+#    --beta_schedule scaled_linear \
+#    --image_size 128 \
+#    --num_epochs 500 \
+#    --num_train_timesteps 1000 \
+#    --num_inference_steps 1000 \
+#    --train_batch_size 64 \
+#    --eval_batch_size 64 \
+#    --wandb_run_name liang_latent_unet_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.5_bs64 \
+#    --calculate_fid \
+#    --calculate_is \
+#    --no_confirm
+#    --no_wandb
+
 python main.py \
     --dataset face \
-    --model latent_unet \
+    --model latelatent_unet_xlnt_unet \
     --pipeline ldmp \
     --scheduler ddim \
     --beta_schedule scaled_linear \
@@ -88,7 +106,7 @@ python main.py \
     --num_inference_steps 1000 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
-    --wandb_run_name liang_latent_unet_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.5_bs64 \
+    --wandb_run_name liang_latent_unet_xl_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.1_bs64 \
     --calculate_fid \
     --calculate_is \
     --no_confirm
