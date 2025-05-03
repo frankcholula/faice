d9bccc5983ebebddd7497f1d11a0d2964c68909c
Ngene <hplxg@hotmail.com>
2025-05-03 16:09:35 +0100

    test: latent_unet_xl_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.1

--- code/experiments/dit.sh
+++ code/experiments/dit.sh
@@ -31,8 +31,8 @@ python main.py \
     --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 256 \
-    --eval_batch_size 256 \
+    --train_batch_size 218 \
+    --eval_batch_size 218 \
     --wandb_run_name liang_transformer_2d_xformers_fast_ddpm_linear_layers4 \
     --calculate_fid \
     --calculate_is \
