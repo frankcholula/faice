99af776cb1a9913ffb0fd931ad1d11bc22da9949
Ngene <hplxg@hotmail.com>
2025-05-03 14:50:01 +0100

    vae_xl_latent_channels4_loss_weight_0.25_batch

--- code/experiments/vae.sh
+++ code/experiments/vae.sh
@@ -3,28 +3,28 @@
 wandb sync --clean-old-hours 1
 wandb artifact cache cleanup 0GB --remove-temp
 
-python main.py \
-    --dataset face \
-    --model vae \
-    --pipeline vae \
-    --image_size 128 \
-    --num_epochs 500 \
-    --train_batch_size 112 \
-    --eval_batch_size 112 \
-    --wandb_run_name liang_vae_latent_channels16_batch_size_112 \
-    --calculate_fid \
-    --no_confirm
-#    --no_wandb
-
 #python main.py \
 #    --dataset face \
-#    --model vae_xl \
+#    --model vae \
 #    --pipeline vae \
 #    --image_size 128 \
 #    --num_epochs 500 \
 #    --train_batch_size 112 \
 #    --eval_batch_size 112 \
-#    --wandb_run_name liang_vae_xl_latent_channels4_batch_size_112 \
+#    --wandb_run_name liang_vae_latent_channels16_batch_size_112 \
 #    --calculate_fid \
-#    --no_confirm \
+#    --no_confirm
+#    --no_wandb
+
+python main.py \
+    --dataset face \
+    --model vae_xl \
+    --pipeline vae \
+    --image_size 128 \
+    --num_epochs 500 \
+    --train_batch_size 112 \
+    --eval_batch_size 112 \
+    --wandb_run_name liang_vae_xl_latent_channels4_loss_weight_0.25_batch_size_112 \
+    --calculate_fid \
+    --no_confirm
 #    --no_wandb
