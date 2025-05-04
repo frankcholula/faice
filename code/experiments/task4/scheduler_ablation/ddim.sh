d464f4c511034bc67611df4d5c0b28c7dee90656
Syrinx <syrinx_w@163.com>
2025-05-04 12:30:03 +0100

    try v with cos

--- code/experiments/task4/scheduler_ablation/ddim.sh
+++ code/experiments/task4/scheduler_ablation/ddim.sh
@@ -11,13 +11,35 @@ python main.py \
     --unet_variant adm \
     --attention_head_dim 64 \
     --prediction_type v_prediction \
+    --image_size 128 \
+    --num_epochs 500 \
+    --num_train_timesteps 4000 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name task4_ddim_cos_v \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
+
+python main.py \
+    --dataset face \
+    --scheduler ddim \
+    --pipeline ddim \
+    --eta 0.0 \
+    --num_inference_steps 100 \
+    --beta_schedule squaredcos_cap_v2 \
+    --model unet \
+    --unet_variant adm \
+    --attention_head_dim 64 \
+    --prediction_type epsilon \
     --rescale_betas_zero_snr \
     --image_size 128 \
     --num_epochs 500 \
     --num_train_timesteps 4000 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
-    --wandb_run_name task4_ddim_100 \
+    --wandb_run_name task4_ddim_cos_0snr \
     --calculate_fid \
     --calculate_is \
-    --verbose
+    --no_confirm
+
