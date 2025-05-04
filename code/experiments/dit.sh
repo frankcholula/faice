994e1c059f0ad2f902126055b63f44373e54b139
Ngene <hplxg@hotmail.com>
2025-05-04 09:41:46 +0100

    test: DiT_B_2_vae_layers4_ddim_scaled_linear_bs64

--- code/experiments/dit.sh
+++ code/experiments/dit.sh
@@ -31,8 +31,8 @@ python main.py \
     --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 8 \
-    --eval_batch_size 8 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --wandb_run_name liang_DiT_B_2_ddpm_linear_bs128 \
     --calculate_fid \
     --calculate_is \
