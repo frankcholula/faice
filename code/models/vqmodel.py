860d8aafd36bf0a33e1e7c7d656893d19f219af0
Ngene <hplxg@hotmail.com>
2025-05-04 18:41:15 +0100

    test: vqvae_latent_loss_weight_0.15_channels3

--- code/models/vqmodel.py
+++ code/models/vqmodel.py
@@ -35,30 +35,7 @@ def base_vqvae(config, latent_channels=3):
 
 
 def vqvae_b_3(config):
-    # return base_vqvae(config, latent_channels=3)
-    vqvae = VQModel(
-        sample_size=config.image_size,  # the target image resolution
-        in_channels=3,  # RGB images
-        out_channels=3,
-        layers_per_block=2,  # how many ResNet layers to use per UNet block
-        block_out_channels=(128, 256, 512),
-        latent_channels=3,
-        num_vq_embeddings=8192,  # Codebook size
-        # scaling_factor=1,
-        # the number of output channels for each UNet block
-        down_block_types=(
-            "DownEncoderBlock2D",  # a regular ResNet downsampling block
-            "DownEncoderBlock2D",
-            "DownEncoderBlock2D",
-        ),
-        up_block_types=(
-            "UpDecoderBlock2D",
-            "UpDecoderBlock2D",
-            "UpDecoderBlock2D",
-        ),
-    )
-
-    return vqvae
+    return base_vqvae(config, latent_channels=3)
 
 
 def vqvae_b_16(config):
