77b83d4c906f11902850fe9aa06aacf0ff0f4191
Syrinx <syrinx_w@163.com>
2025-05-05 02:53:08 +0100

    t4 baseline num_train_timesteps=4000, losstype sh

--- code/experiments/task4/task4_baseline.sh
+++ code/experiments/task4/task4_baseline.sh
@@ -9,7 +9,7 @@ python main.py \
     --attention_head_dim 64 \
     --image_size 128 \
     --num_epochs 500 \
-    --num_train_timesteps 1000 \
+    --num_train_timesteps 4000 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
     --wandb_run_name task4_baseline \
