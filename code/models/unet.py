8d1746f7bb6908d09b1880f6aab7eb10eef53f04
frankcholula <lu.phrank@gmail.com>
2025-05-05 01:22:34 +0100

    perhaps a confusion on my part.

--- code/models/unet.py
+++ code/models/unet.py
@@ -1,4 +1,4 @@
-from diffusers import UNet2DModel
+from diffusers import UNet2DModel, UNet2DConditionModel
 
 
 class BaseUNet(UNet2DModel):
@@ -112,31 +112,59 @@ class ADMUNet(UNet2DModel):
             resnet_time_scale_shift="scale_shift",  # This is the AdaGN portion.
             block_out_channels=(128, 128, 256, 256, 512, 512),
             down_block_types=(
-                "DownBlock2D",  # 128 -> 64
+                "ResnetDownsampleBlock2D",  # 128 -> 64
                 "AttnDownBlock2D",  # 64 -> 32  (2 attention heads)
                 "AttnDownBlock2D",  # 32 -> 16 (4 attention heads)
                 "AttnDownBlock2D",  # 16 -> 8 (8 attention heads)
-                "DownBlock2D",  # 8 -> 4
-                "DownBlock2D",  # 4 -> 2
+                "ResnetDownsampleBlock2D",  # 8 -> 4
+                "ResnetDownsampleBlock2D",  # 4 -> 2
             ),
             up_block_types=(
-                "UpBlock2D",  # 2 -> 4
+                "ResnetUpsampleBlock2D",  # 2 -> 4
                 "AttnUpBlock2D",  # 4 -> 8 (8 attention heads)
                 "AttnUpBlock2D",  # 8 -> 16 (4 attention heads)
                 "AttnUpBlock2D",  # 16 -> 32 (2 attention heads)
-                "UpBlock2D",  # 32 -> 64
-                "UpBlock2D",  # 64 -> 128
+                "ResnetUpsampleBlock2D",  # 32 -> 64
+                "ResnetUpsampleBlock2D",  # 64 -> 128
             ),
         )
 
 
-
+# class ConditionalUNet(UNet2DConditionModel):
+#     def __init__(self, config):
+#         super().__init__(
+#             sample_size=config.image_size,
+#             in_channels=3,
+#             out_channels=3,
+#             layers_per_block=2,
+#             block_out_channels=(128, 128, 256, 256, 512, 512),
+#             down_block_types=(
+#                 "DownBlock2D",  # 128 -> 64
+#                 "AttnDownBlock2D",  # 64 -> 32
+#                 "AttnDownBlock2D",  # 32 -> 16
+#                 "AttnDownBlock2D",  # 16 -> 8
+#                 "DownBlock2D",  # 8 -> 4
+#                 "DownBlock2D",  # 4 -> 2
+#             ),
+#             up_block_types=(
+#                 "UpBlock2D",  # 2 -> 4
+#                 "AttnUpBlock2D",  # 4 -> 8
+#                 "AttnUpBlock2D",  # 8 -> 16
+#                 "AttnUpBlock2D",  # 16 -> 32
+#                 "UpBlock2D",  # 32 -> 64
+#                 "UpBlock2D",  # 64 -> 128
+#             ),
+#             attention_head_dim=64,
+#             cross_attention_dim=config.cross_attention_dim,
+#             resnet_time_scale_shift="scale_shift",
+#         )
 
 
 ARCHITECTURES = {
     "base": BaseUNet,
     "ddpm": DDPMUNet,
     "adm": ADMUNet,
+    # "cond": ConditionalUNet,
 }
 
 
