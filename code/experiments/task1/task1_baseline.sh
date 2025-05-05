dc1547f256e736934a2842e515c10df898fff318
frankcholula <lu.phrank@gmail.com>
2025-05-05 01:30:15 +0100

    tried running with resnet block.

--- code/experiments/task1/task1_baseline.sh
+++ code/experiments/task1/task1_baseline.sh
@@ -15,7 +15,7 @@ python main.py \
     --num_epochs 500 \
     --train_batch_size 24 \
     --eval_batch_size 24 \
-    --wandb_run_name task1_baseline \
+    --wandb_run_name task1_baseline_resnet_block \
     --calculate_fid \
     --calculate_is \
     --verbose
