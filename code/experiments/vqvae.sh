5d95890188a8380eb63b567fbfd315f0594bca7b
Ngene <hplxg@hotmail.com>
2025-05-05 12:29:41 +0100

    test: vqvae_latent_loss_weight_0.05_channels3

--- code/experiments/vqvae.sh
+++ code/experiments/vqvae.sh
@@ -25,7 +25,7 @@ python main.py \
     --num_epochs 500 \
     --train_batch_size 112 \
     --eval_batch_size 112 \
-    --wandb_run_name liang_vqvae_latent_loss_weight_0.15_channels3_batch_size_112 \
+    --wandb_run_name liang_vqvae_latent_loss_weight_0.05_channels3_batch_size_112 \
     --calculate_fid \
     --no_confirm
 #    --no_wandb
