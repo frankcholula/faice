62f759a4c83aa33706756354b211661e737a7b9f
frankcholula <lu.phrank@gmail.com>
2025-05-05 18:18:04 +0100

    added female.

---
+++ code/experiments/task5/guidance_female.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python main.py \
+    --dataset face \
+    --scheduler ddpm \
+    --pipeline cond \
+    --beta_schedule linear \
+    --model unet \
+    --unet_variant cond \
+    --conditioning_on female \
+    --attention_head_dim 64 \
+    --image_size 128 \
+    --num_epochs 500 \
+    --num_train_timesteps 1000 \
+    --train_batch_size 24 \
+    --eval_batch_size 24 \
+    --wandb_run_name task5_baseline \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
