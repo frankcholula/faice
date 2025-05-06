06c693d142ef3e393f2f244b2833558bcb81177f
Ngene <hplxg@hotmail.com>
2025-05-06 08:57:21 +0100

    feat: add data augmentation: center_crop_arr

--- code/utils/transforms.py
+++ code/utils/transforms.py
@@ -10,6 +10,7 @@ def build_transforms(config):
         T.Resize((config.image_size, config.image_size)),
         T.ToTensor(),
         T.Normalize([0.5], [0.5]),
+        # T.Lambda(lambda pil_image: center_crop_arr(pil_image, args.image_size)),
     ]
 
     transform_test = [
@@ -26,3 +27,24 @@ def build_transforms(config):
     transform_test = T.Compose(transform_test)
 
     return transform_train, transform_test
+
+
+def center_crop_arr(pil_image, image_size):
+    """
+    Center cropping implementation from ADM.
+    https://github.com/openai/guided-diffusion/blob/8fb3ad9197f16bbc40620447b2742e13458d2831/guided_diffusion/image_datasets.py#L126
+    """
+    while min(*pil_image.size) >= 2 * image_size:
+        pil_image = pil_image.resize(
+            tuple(x // 2 for x in pil_image.size), resample=Image.BOX
+        )
+
+    scale = image_size / min(*pil_image.size)
+    pil_image = pil_image.resize(
+        tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC
+    )
+
+    arr = np.array(pil_image)
+    crop_y = (arr.shape[0] - image_size) // 2
+    crop_x = (arr.shape[1] - image_size) // 2
+    return Image.fromarray(arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size])
