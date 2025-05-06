4b35ab3c441190b8f0aff4e5deaafda7c8a58780
Syrinx <syrinx_w@163.com>
2025-05-06 03:48:17 +0100

    bs16

--- code/experiments/task6/lpips_net/mse_alex.sh
+++ code/experiments/task6/lpips_net/mse_alex.sh
@@ -11,8 +11,8 @@ python main.py \
     --image_size 128 \
     --num_epochs 500 \
     --num_train_timesteps 4000 \
-    --train_batch_size 24 \
-    --eval_batch_size 24 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --loss_type mse \
     --use_lpips \
     --lpips_net alex \
