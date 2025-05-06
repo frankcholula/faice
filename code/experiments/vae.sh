cdf146abbeab71cd2a0870e254277da53972c9d1
Ngene <hplxg@hotmail.com>
2025-05-06 09:12:15 +0100

    feat: vae_l_4_latent_channels4_loss_weight_0.5_batch_size_16

--- code/experiments/vae.sh
+++ code/experiments/vae.sh
@@ -24,7 +24,7 @@ python main.py \
     --num_epochs 500 \
     --train_batch_size 16 \
     --eval_batch_size 16 \
-    --wandb_run_name liang_vae_l_4_latent_channels4_loss_weight_0.1_batch_size_16 \
+    --wandb_run_name liang_vae_l_4_latent_channels4_loss_weight_0.5_batch_size_16 \
     --calculate_fid \
     --no_confirm
 #    --no_wandb
