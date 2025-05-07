c29bece87bff0d7a0eb583357be07d38b11cd579
Ngene <hplxg@hotmail.com>
2025-05-07 10:47:54 +0100

    test: enable_xformers_memory_efficient_attention for ldmp

--- code/experiments/task3/ldmp.sh
+++ code/experiments/task3/ldmp.sh
@@ -160,12 +160,12 @@ python main.py \
     --num_inference_steps 1000 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
-    --wandb_run_name liang_unet_l_block_6_head_dim_64_layer_4_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+    --wandb_run_name liang_unet_l_block_6_head_dim_64_layer_4_xformers_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
     --calculate_fid \
     --calculate_is \
     --enable_xformers_memory_efficient_attention \
-    --no_confirm \
-    --no_wandb
+    --no_confirm
+#    --no_wandb
 
 
 #python main.py \
