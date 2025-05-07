3f3dbafd846964f353139420898ff1e327380a23
Ngene <hplxg@hotmail.com>
2025-05-07 08:28:24 +0100

    test: unet_l_block_8_head_dim_64_layer_4_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64

--- code/experiments/task3/ldmp.sh
+++ code/experiments/task3/ldmp.sh
@@ -148,9 +148,28 @@ wandb artifact cache cleanup 0GB --remove-temp
 #    --no_confirm
 #    --no_wandb
 
+#python main.py \
+#    --dataset face \
+#    --model unet_l_block_6_head_dim_64_layer_4 \
+#    --pipeline ldmp \
+#    --scheduler ddim \
+#    --beta_schedule scaled_linear \
+#    --image_size 128 \
+#    --num_epochs 500 \
+#    --num_train_timesteps 1000 \
+#    --num_inference_steps 1000 \
+#    --train_batch_size 64 \
+#    --eval_batch_size 64 \
+#    --wandb_run_name liang_unet_l_block_6_head_dim_64_layer_4_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+#    --calculate_fid \
+#    --calculate_is \
+#    --no_confirm
+#    --no_wandb
+
+
 python main.py \
     --dataset face \
-    --model unet_l_block_6_head_dim_64_layer_4 \
+    --model unet_l_block_8_head_dim_64_layer_4 \
     --pipeline ldmp \
     --scheduler ddim \
     --beta_schedule scaled_linear \
@@ -160,7 +179,7 @@ python main.py \
     --num_inference_steps 1000 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
-    --wandb_run_name liang_unet_l_block_6_head_dim_64_layer_4_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+    --wandb_run_name liang_unet_l_block_8_head_dim_64_layer_4_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
     --calculate_fid \
     --calculate_is \
     --no_confirm
