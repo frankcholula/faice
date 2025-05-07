60167d0cd32e7b29930d4c1350f18bbc8b6040dd
Ngene <hplxg@hotmail.com>
2025-05-07 09:44:30 +0100

    test: unet_xl_block_6_head_dim_64_layer_4_ldmp_ddim_scaled_linear_vqvae3

--- code/experiments/task3/ldmp.sh
+++ code/experiments/task3/ldmp.sh
@@ -174,7 +174,7 @@ python main.py \
     --scheduler ddim \
     --beta_schedule scaled_linear \
     --image_size 128 \
-    --num_epochs 1 \
+    --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
     --train_batch_size 64 \
@@ -182,5 +182,5 @@ python main.py \
     --wandb_run_name liang_unet_xl_block_6_head_dim_64_layer_4_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
     --calculate_fid \
     --calculate_is \
-    --no_confirm \
-    --no_wandb
+    --no_confirm
+#    --no_wandb
