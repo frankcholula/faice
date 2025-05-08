a71c8119cd2f664a1f0cd53a96ba22d46ab597dd
Syrinx <syrinx_w@163.com>
2025-05-08 13:22:59 +0100

    sh for hybrid weight

---
+++ code/experiments/task6/hybrid_weight/0.5_hybrid_weight.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python main.py \
+    --dataset face \
+    --scheduler ddpm \
+    --pipeline ddpm \
+    --beta_schedule linear \
+    --model unet \
+    --unet_variant adm \
+    --attention_head_dim 64 \
+    --image_size 128 \
+    --num_epochs 500 \
+    --num_train_timesteps 4000 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --loss_type hybrid \
+    --hybrid_weight 0.5 \
+    --wandb_run_name task6_hybrid_0.5 \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
