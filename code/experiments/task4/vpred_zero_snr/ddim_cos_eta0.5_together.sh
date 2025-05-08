5fa2b3334f67242c169bcf06e60e1d90e89918d1
Syrinx <syrinx_w@163.com>
2025-05-08 16:38:34 +0100

    ddim_cos_eta0.5 v&0snr

---
+++ code/experiments/task4/vpred_zero_snr/ddim_cos_eta0.5_together.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+python main.py \
+    --dataset face \
+    --scheduler ddim \
+    --pipeline ddim \
+    --num_inference_steps 100 \
+    --beta_schedule squaredcos_cap_v2 \
+    --model unet \
+    --unet_variant adm \
+    --attention_head_dim 64 \
+    --eta 0.5 \
+    --prediction_type v_prediction \
+    --rescale_betas_zero_snr \
+    --image_size 128 \
+    --num_epochs 500 \
+    --num_train_timesteps 4000 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
+    --wandb_run_name task4_ddim_cos_eta0.5_v_0snr \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
