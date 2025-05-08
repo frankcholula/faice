68db5682a9286de20c2942c2c4cbe11674e79554
Ngene <hplxg@hotmail.com>
2025-05-08 19:21:33 +0100

    test: DiT_B_2_vae_channels_4_compress_4_ddim_linear_bs64_vae0.05

--- code/experiments/task2/dit_vae.sh
+++ code/experiments/task2/dit_vae.sh
@@ -126,9 +126,9 @@ python main.py \
     --num_epochs 1 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
-    --wandb_run_name liang_DiT_B_2_vae_channels_4_compress_4_ddim_linear_bs64_vae0.05 \
+    --train_batch_size 32 \
+    --eval_batch_size 32 \
+    --wandb_run_name liang_DiT_B_2_vae_channels_4_compress_4_ddim_linear_bs32_vae0.05 \
     --calculate_fid \
     --calculate_is \
     --enable_xformers_memory_efficient_attention \
