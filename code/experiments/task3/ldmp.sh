1ec15e9a39d94493d06e01316d369551c025bc24
Ngene <hplxg@hotmail.com>
2025-05-08 11:38:20 +0100

    test: unet_l_block_5_head_dim_64_train_timesteps_4000_RHFlip_center_crop_arr_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64

--- code/experiments/task3/ldmp.sh
+++ code/experiments/task3/ldmp.sh
@@ -130,29 +130,6 @@ wandb artifact cache cleanup 0GB --remove-temp
 #    --no_confirm \
 #    --no_wandb
 
-python main.py \
-    --dataset face \
-    --model unet_l_block_5_head_dim_64 \
-    --pipeline ldmp \
-    --scheduler ddim \
-    --beta_schedule scaled_linear \
-    --eta 0.5 \
-    --RHFlip \
-    --center_crop_arr \
-    --image_size 128 \
-    --num_epochs 500 \
-    --num_train_timesteps 4000 \
-    --num_inference_steps 1000 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
-    --wandb_run_name liang_ema_unet_l_block_5_head_dim_64_train_timesteps_4000_eta_0.5_RHFlip_center_crop_arr_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
-    --calculate_fid \
-    --calculate_is \
-    --enable_xformers_memory_efficient_attention \
-    --allow_tf32 \
-    --use_ema \
-    --no_confirm
-#    --no_wandb
 
 #python main.py \
 #    --dataset face \
@@ -193,4 +170,51 @@ python main.py \
 #    --enable_xformers_memory_efficient_attention \
 #    --allow_tf32 \
 #    --no_confirm
+#    --no_wandb
+
+#python main.py \
+#    --dataset face \
+#    --model unet_l_block_5_head_dim_64 \
+#    --pipeline ldmp \
+#    --scheduler ddim \
+#    --beta_schedule scaled_linear \
+#    --eta 0.5 \
+#    --RHFlip \
+#    --center_crop_arr \
+#    --image_size 128 \
+#    --num_epochs 500 \
+#    --num_train_timesteps 4000 \
+#    --num_inference_steps 1000 \
+#    --train_batch_size 64 \
+#    --eval_batch_size 64 \
+#    --wandb_run_name liang_ema_unet_l_block_5_head_dim_64_train_timesteps_4000_eta_0.5_RHFlip_center_crop_arr_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+#    --calculate_fid \
+#    --calculate_is \
+#    --enable_xformers_memory_efficient_attention \
+#    --allow_tf32 \
+#    --use_ema \
+#    --no_confirm
+#    --no_wandb
+
+
+python main.py \
+    --dataset face \
+    --model unet_l_block_5_head_dim_64 \
+    --pipeline ldmp \
+    --scheduler ddim \
+    --beta_schedule scaled_linear \
+    --RHFlip \
+    --center_crop_arr \
+    --image_size 128 \
+    --num_epochs 500 \
+    --num_train_timesteps 4000 \
+    --num_inference_steps 1000 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name liang_unet_l_block_5_head_dim_64_train_timesteps_4000_RHFlip_center_crop_arr_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+    --calculate_fid \
+    --calculate_is \
+    --enable_xformers_memory_efficient_attention \
+    --allow_tf32 \
+    --no_confirm
 #    --no_wandb
