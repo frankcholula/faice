a8b712f03a3ca25fb193d3b8ce1230a61abd77c8
Ngene <hplxg@hotmail.com>
2025-05-08 08:53:37 +0100

    test: unet_l_block_5_head_dim_64_train_timesteps_4000_eta_0.5_RHFlip_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16

--- code/experiments/task3/ldmp.sh
+++ code/experiments/task3/ldmp.sh
@@ -130,9 +130,30 @@ wandb artifact cache cleanup 0GB --remove-temp
 #    --no_confirm \
 #    --no_wandb
 
+python main.py \
+    --dataset face \
+    --model unet_l_block_5_head_dim_64 \
+    --pipeline ldmp \
+    --scheduler ddim \
+    --beta_schedule scaled_linear \
+    --eta 0.5 \
+    --RHFlip \
+    --image_size 128 \
+    --num_epochs 500 \
+    --num_train_timesteps 4000 \
+    --num_inference_steps 1000 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name liang_unet_l_block_5_head_dim_64_train_timesteps_4000_eta_0.5_RHFlip_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+    --calculate_fid \
+    --calculate_is \
+    --enable_xformers_memory_efficient_attention \
+    --no_confirm
+#    --no_wandb
+
 #python main.py \
 #    --dataset face \
-#    --model unet_l_block_5_head_dim_64 \
+#    --model unet_l_block_6_head_dim_64 \
 #    --pipeline ldmp \
 #    --scheduler ddim \
 #    --beta_schedule scaled_linear \
@@ -142,30 +163,12 @@ wandb artifact cache cleanup 0GB --remove-temp
 #    --num_inference_steps 1000 \
 #    --train_batch_size 64 \
 #    --eval_batch_size 64 \
-#    --wandb_run_name liang_unet_l_block_5_head_dim_64_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+#    --wandb_run_name liang_unet_l_block_6_head_dim_64_xformers_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
 #    --calculate_fid \
 #    --calculate_is \
+#    --enable_xformers_memory_efficient_attention \
+#    --allow_tf32 \
 #    --no_confirm
-#    --no_wandb
-
-python main.py \
-    --dataset face \
-    --model unet_l_block_6_head_dim_64 \
-    --pipeline ldmp \
-    --scheduler ddim \
-    --beta_schedule scaled_linear \
-    --image_size 128 \
-    --num_epochs 500 \
-    --num_train_timesteps 1000 \
-    --num_inference_steps 1000 \
-    --train_batch_size 64 \
-    --eval_batch_size 64 \
-    --wandb_run_name liang_unet_l_block_6_head_dim_64_xformers_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
-    --calculate_fid \
-    --calculate_is \
-    --enable_xformers_memory_efficient_attention \
-    --allow_tf32 \
-    --no_confirm
 #    --no_wandb
 
 
