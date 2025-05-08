d80b90575f8b32c32e3473e7627b867ad0dd48a7
Ngene <hplxg@hotmail.com>
2025-05-08 09:42:16 +0100

    test: unet_l_block_5_head_dim_64_train_timesteps_4000_eta_0.5_RHFlip_new_center_crop_arr

--- code/experiments/task3/ldmp.sh
+++ code/experiments/task3/ldmp.sh
@@ -145,12 +145,12 @@ python main.py \
     --num_inference_steps 1000 \
     --train_batch_size 64 \
     --eval_batch_size 64 \
-    --wandb_run_name liang_unet_l_block_5_head_dim_64_train_timesteps_4000_eta_0.5_RHFlip_new_center_crop_arr_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
+    --wandb_run_name liang_unet_l_block_5_head_dim_64_train_timesteps_4000_eta_0.5_RHFlip_center_crop_arr_ldmp_ddim_scaled_linear_vqvae3_loss_weight0.4bs16_bs64 \
     --calculate_fid \
     --calculate_is \
     --enable_xformers_memory_efficient_attention \
-    --no_confirm \
-    --no_wandb
+    --no_confirm
+#    --no_wandb
 
 #python main.py \
 #    --dataset face \
--- code/utils/transforms.py
+++ code/utils/transforms.py
@@ -23,31 +23,9 @@ def build_transforms(config):
     if config.gblur:
         transform_train += [T.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))]
     if config.center_crop_arr:
-        transform_train += [T.Lambda(lambda pil_image: center_crop_arr(pil_image, config.image_size))]
-        # transform_train += [T.CenterCrop((config.image_size, config.image_size))]
+        transform_train += [T.CenterCrop((config.image_size, config.image_size))]
 
     transform_train = T.Compose(transform_train)
     transform_test = T.Compose(transform_test)
 
     return transform_train, transform_test
-
-
-def center_crop_arr(pil_image, image_size):
-    """
-    Center cropping implementation from ADM.
-    https://github.com/openai/guided-diffusion/blob/8fb3ad9197f16bbc40620447b2742e13458d2831/guided_diffusion/image_datasets.py#L126
-    """
-    if min(*pil_image.size()) >= 2 * image_size:
-        pil_image = pil_image.resize(
-            tuple(x // 2 for x in pil_image.size()), resample=Image.BOX
-        )
-
-    scale = image_size / min(*pil_image.size())
-    pil_image = pil_image.resize(
-        tuple(round(x * scale) for x in pil_image.size()), resample=Image.BICUBIC
-    )
-
-    arr = np.array(pil_image)
-    crop_y = (arr.shape[0] - image_size) // 2
-    crop_x = (arr.shape[1] - image_size) // 2
-    return Image.fromarray(arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size])
