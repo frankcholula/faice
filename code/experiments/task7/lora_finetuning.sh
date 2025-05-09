173ace66304638deb8c227831deafad7717adc64
Frank Lu <lu.phrank@gmail.com>
2025-05-09 18:50:25 +0100

    fix commands.

--- code/experiments/task7/lora_finetuning.sh
+++ code/experiments/task7/lora_finetuning.sh
@@ -1,16 +1,14 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export DATASET_NAME="code/datasets/celeba_hq_stable_diffusion"
+export DATASET_NAME="Ryan-sjtu/celebahq-caption"
 
-accelerate launch --mixed_precision="fp16" pipelines/train_text_to_image_lora.py \
+accelerate launch --mixed_precision="bf16" pipelines/train_text_to_image_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --dataset_name=$DATASET_NAME --caption_column="text" \
   --resolution=512 --random_flip \
-  --train_batch_size=1 \
-  --num_train_epochs=25 --checkpointing_steps=5000 \
+  --train_batch_size=8 \
+  --num_train_epochs=10 --checkpointing_steps=5000 \
   --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --seed=42 \
   --output_dir="test_lora-model" \
-  --validation_prompt="sexy woman with big jugs" --report_to="wandb" \
-  --use_peft \
-  --lora_r=4 --lora_alpha=32 \
-  --lora_text_encoder_r=4 --lora_text_encoder_alpha=32
+  --validation_prompt="sexy woman with big jugs" --report_to="wandb" 
+  
