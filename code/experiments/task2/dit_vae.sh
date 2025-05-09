2e22f9201755bdb89c7545a8dc03e48585279620
Ngene <hplxg@hotmail.com>
2025-05-09 23:45:07 +0100

    test: pretain_ddim_scaled_linear_bs32_vae_l_4_0.05

--- code/experiments/task2/dit_vae.sh
+++ code/experiments/task2/dit_vae.sh
@@ -171,14 +171,14 @@ python main.py \
     --num_epochs 500 \
     --num_train_timesteps 1000 \
     --num_inference_steps 1000 \
-    --train_batch_size 32 \
-    --eval_batch_size 32 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
     --wandb_run_name liang_pretain_ddim_scaled_linear_bs32_vae_l_4_0.05 \
     --calculate_fid \
     --calculate_is \
     --enable_xformers_memory_efficient_attention \
     --allow_tf32 \
-    --no_confirm
-#    --no_wandb
+    --no_confirm \
+    --no_wandb
 
 
