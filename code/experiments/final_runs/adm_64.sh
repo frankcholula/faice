26ccb9b93aff54039d101b5c0decdf893f4d14a3
Syrinx <syrinx_w@163.com>
2025-05-10 00:36:52 +0100

    try 50

---
+++ code/experiments/final_runs/adm_64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+python main.py \
+    --dataset face \
+    --scheduler ddim \
+    --pipeline ddim \
+    --beta_schedule squaredcos_cap_v2 \
+    --eta 0.5 \
+    --prediction_type v_prediction \
+    --model unet \
+    --unet_variant adm \
+    --attention_head_dim 64 \
+    --RHFlip \
+    --center_crop_arr \
+    --image_size 128 \
+    --num_epochs 50 \
+    --num_train_timesteps 4000 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --loss_type mse \
+    --use_lpips \
+    --lpips_net alex \
+    --lpips_weight 0.05 \
+    --wandb_run_name fixbug_50 \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
