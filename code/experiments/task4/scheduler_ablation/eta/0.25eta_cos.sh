44c516a0d3622e6b92c6ea7eb80770ea925cca76
Syrinx <syrinx_w@163.com>
2025-05-10 18:16:02 +0100

    frun for eta and v 0snr

---
+++ code/experiments/task4/scheduler_ablation/eta/0.25eta_cos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python main.py \
+    --dataset face \
+    --scheduler ddim \
+    --pipeline ddim \
+    --eta 0.25 \
+    --num_inference_steps 100 \
+    --beta_schedule squaredcos_cap_v2 \
+    --model unet \
+    --unet_variant adm \
+    --attention_head_dim 64 \
+    --image_size 128 \
+    --num_epochs 500 \
+    --num_train_timesteps 4000 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --wandb_run_name task4_ddim_cos_0.25eta \
+    --calculate_fid \
+    --calculate_is \
+    --verbose
