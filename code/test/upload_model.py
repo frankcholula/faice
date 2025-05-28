677b6e192a3d3b19f363a0b5f447b3d016ba9cfa
Ngene <hplxg@hotmail.com>
2025-05-28 11:51:04 +0100

    feat: add token for upload_model

--- code/test/upload_model.py
+++ code/test/upload_model.py
@@ -6,24 +6,35 @@
 @Project : code
 """
 import os
-from pathlib import Path
-from utils.metrics import get_full_repo_name
-from huggingface_hub import Repository
+from huggingface_hub import HfApi
+
+api = HfApi()
+
+
+def upload_model(repo_id, local_dir, token):
+    """
+    Upload a model to the Hugging Face Hub.
+
+    Args:
+        repo_id (str): The repository ID of the model.
+        local_dir (str): The local directory containing the model files.
+        token (str): The authentication token for the Hugging Face Hub.
+
+    Returns:
+        None
+    """
+    api.upload_folder(
+        repo_id=repo_id,
+        folder_path=local_dir,
+        token=token,
+    )
 
 
 class Config():
-    use_wandb = False
-    num_inference_steps = 100
-    seed = 0
     output_dir = "runs/unet_cond_l_block_4-stable_diffusion-pndm-face_dialog-200"
 
 
 config = Config()
 token = os.environ.get("HUGGINGFACE_TOKEN")
 
-model_name = Path(config.output_dir).name
-repo_name = get_full_repo_name(model_name, token=token)
-repo = Repository(config.output_dir, clone_from=repo_name)
-
-repo.push_to_hub(commit_message=f"upload model: {model_name}", blocking=True)
-
+upload_model(repo_id="Ngene787/Faice_text2face", local_dir=config.output_dir, token=token)
